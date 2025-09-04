a8bce7e86ff8326332e850ac8ae76a555209ba9e
karrar mohammed <karrar.m.habeeb@gmail.com>
2025-09-04 17:35:17 +0300

    happy init commit

---
+++ src/main/kotlin/net/thechance/mena/MenaApplication.kt
@@ -0,0 +1,11 @@
+package net.thechance.mena
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MenaApplication
+
+fun main(args: Array<String>) {
+    runApplication<MenaApplication>(*args)
+}
---
+++ src/test/kotlin/net/thechance/mena/MenaApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.thechance.mena
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MenaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
