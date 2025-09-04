d36cb19d938e47e8e8af4388640cb5f34107bde9
karrar mohammed <karrar.m.habeeb@gmail.com>
2025-09-04 17:49:58 +0300

    implement hello controller

--- src/main/kotlin/net/thechance/mena/MenaApplication.kt
+++ src/main/kotlin/net/thechance/mena/MenaApplication.kt
@@ -2,10 +2,21 @@ package net.thechance.mena
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class MenaApplication
 
 fun main(args: Array<String>) {
     runApplication<MenaApplication>(*args)
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello MENA World!"
+    }
 }
