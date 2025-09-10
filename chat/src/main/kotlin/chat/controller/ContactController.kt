14da6539cc2deff683b9956ee60ac294d10dd5ed
omer1998 <omerfaris11@gmail.com>
2025-09-10 17:55:42 +0300

    feat: implement get contact end point

--- chat/src/main/kotlin/chat/controller/ContactController.kt
+++ chat/src/main/kotlin/chat/controller/ContactController.kt
@@ -1,16 +1,24 @@
 package net.thechance.chat.controller
 
+import chat.dto.PagedResponse
+import net.thechance.chat.entity.Contact
 import net.thechance.chat.service.ContactService
+import net.thechance.mena.mapper.toPagedResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/contact")
-class ContactController (private val contactService : ContactService) {
+class ContactController(private val contactService: ContactService) {
 
-    @GetMapping("/hello")
-    fun hello(): String{
-        return "hello"
+    @GetMapping
+    fun getContacts(
+        @RequestParam pageNumber: Int,
+        @RequestParam pageSize: Int,
+    ): PagedResponse<Contact> {
+        val pageResult = contactService.getAllContacts(pageNumber = pageNumber, pageSize = pageSize)
+        return pageResult.toPagedResponse()
     }
 }
