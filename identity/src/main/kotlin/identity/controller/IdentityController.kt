b5e84a1649e162d153f883f74922ee2197834066
AimanYosofi <AimanYosofiUlt@gmail.com>
2025-09-14 23:36:52 +0300

    fix login endpoint by changing it from get to post

--- identity/src/main/kotlin/identity/controller/IdentityController.kt
+++ identity/src/main/kotlin/identity/controller/IdentityController.kt
@@ -4,8 +4,11 @@ import jakarta.validation.Valid
 import net.thechance.identity.dto.AuthRequest
 import net.thechance.identity.dto.AuthResponse
 import net.thechance.identity.service.AuthenticationService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,8 +18,12 @@ import org.springframework.web.bind.annotation.RestController
 class IdentityController(
     val authenticationService: AuthenticationService
 ) {
+    @ExceptionHandler(BadCredentialsException::class)
+    fun handleBadCredentialsException(): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid phone number or password")
+    }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     fun login(@RequestBody @Valid request: AuthRequest): ResponseEntity<AuthResponse> {
         val authResponse = authenticationService.login(request.phoneNumber, request.password)
         return ResponseEntity.ok(authResponse)
