ddb174ae100ad0b2a06d8ac33463194cc5e6a7c6
Malak Raef <malak.raef20@gmail.com>
2025-09-16 00:56:34 +0300

    refactor: remove repository function and relocate file

---
+++ wallet/src/main/kotlin/net/thechance/wallet/repository/TransactionRepository.kt
@@ -0,0 +1,7 @@
+package net.thechance.wallet.repository
+
+import net.thechance.wallet.entity.Transaction
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface TransactionRepository:JpaRepository<Transaction,UUID>
