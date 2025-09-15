c3ae0028ca61f6b1a41694c3c96b93e1a9f4f0c2
Zyad <zyadhammad531@gmail.com>
2025-09-15 22:23:14 +0300

    fix: column definition for uuid

--- trends/src/main/kotlin/trends/entity/Category.kt
+++ trends/src/main/kotlin/trends/entity/Category.kt
@@ -1,20 +1,29 @@
 package net.thechance.trends.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.util.*
 
 @Table(name = "trends")
 @Entity
 data class Category(
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(
+        columnDefinition = "uuid",
+        nullable = false,
+        updatable = false
+    )
     val id: UUID = UUID.randomUUID(),
+
     @Column(
         name = "name",
         nullable = false,
         unique = true
     )
     val name: String,
+
     @Column(
         name = "emoji",
         nullable = false,
