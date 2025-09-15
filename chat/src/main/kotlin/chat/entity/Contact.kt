48feb5743d5ae3922b1525355eb78b4d8e32ea20
omer1998 <omerfaris11@gmail.com>
2025-09-15 22:59:08 +0300

    refactor: contact entity

--- chat/src/main/kotlin/chat/entity/Contact.kt
+++ chat/src/main/kotlin/chat/entity/Contact.kt
@@ -9,13 +9,10 @@ import java.util.UUID
 @Entity
 @Table(name = "contacts", schema = "chat")
 data class Contact(
-    @Id @Column(
-        columnDefinition = "uuid",
-        updatable = false,
-        nullable = false
-    ) val id: UUID = UUID.randomUUID(),
-    @Column(nullable = false) val name: String,
+    @Id @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    val id: UUID = UUID.randomUUID(),
+    @Column(nullable = false) val firstName: String,
+    @Column(nullable = false) val lastName: String,
     @Column(nullable = false) val phoneNumber: String,
     @Column(nullable = false) val userId: UUID,
-
 )
