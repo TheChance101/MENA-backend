c7209e8e7839a7b39434b1b0d2c16d8374454b23
Malak Raef <malak.raef20@gmail.com>
2025-09-15 03:41:05 +0300

    feat: add Block entity table

---
+++ wallet/src/main/kotlin/wallet/entity/Block.kt
@@ -0,0 +1,19 @@
+package wallet.entity
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+import java.util.*
+
+@Entity
+@Table(name = "blocks")
+data class Block(
+    @Id
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    val id: UUID = UUID.randomUUID(),
+
+    @Column(name = "previous_block_hash", nullable = false, updatable = false)
+    val previousBlockHash: String,
+
+    @Column(nullable = false, updatable = false)
+    val timestamp: LocalDateTime = LocalDateTime.now()
+)
