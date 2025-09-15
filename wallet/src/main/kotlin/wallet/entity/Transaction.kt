f1fe48c63a7de20cb2b2bd931cf21b5e2a2605e3
Malak Raef <malak.raef20@gmail.com>
2025-09-15 03:40:58 +0300

    feat: add Transaction entity table

---
+++ wallet/src/main/kotlin/wallet/entity/Transaction.kt
@@ -0,0 +1,33 @@
+package wallet.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import java.util.*
+
+
+@Entity
+@Table(name = "transactions")
+data class Transaction(
+    @Id
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    val id: UUID = UUID.randomUUID(),
+
+    @Column(nullable = false, updatable = false)
+    val timestamp: LocalDateTime = LocalDateTime.now(),
+
+    @Column(name = "sender_id", columnDefinition = "uuid", nullable = false, updatable = false)
+    val senderId: UUID,
+
+    @Column(name = "receiver_id", columnDefinition = "uuid", nullable = false, updatable = false)
+    val receiverId: UUID,
+
+    @Column(name = "sender_signature", columnDefinition = "TEXT", nullable = false, updatable = false)
+    val senderSignature: String,
+
+    @Column(nullable = false, updatable = false)
+    val amount: BigDecimal
+)
