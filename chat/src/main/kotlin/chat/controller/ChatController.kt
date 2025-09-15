287bb99485696a44efd98b824e22d69705a0e0ec
raouf-kamel <raoufkamel2013@gmail.com>
2025-09-15 16:15:30 +0300

    add basic structure

--- chat/src/main/kotlin/chat/controller/ChatController.kt
+++ chat/src/main/kotlin/chat/controller/ChatController.kt
@@ -1,4 +1,4 @@
-package net.thechance.chat
+package net.thechance.chat.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
