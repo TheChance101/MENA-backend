e0fa8ce00fadf0965dce6afa225413d3994b8811
Fares <dev.faresmohamed@gmail.com>
2025-09-16 17:21:18 +0300

    FTH-13: add unit tests for BookmarkService to validate saveBookmark functionality

---
+++ faith/src/test/kotlin/net/thechance/faith/service/BookmarkServiceTest.kt
@@ -0,0 +1,35 @@
+package net.thechance.faith.service
+
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import io.mockk.mockk
+import net.thechance.faith.entity.Bookmark
+import net.thechance.faith.repository.BookmarkRepository
+import org.junit.Test
+import java.time.Instant
+import java.util.*
+
+class BookmarkServiceTest {
+    private val bookmarkRepository = mockk<BookmarkRepository>()
+    private val bookmarkService = BookmarkService(bookmarkRepository)
+
+    @Test
+    fun `saveBookmark should save bookmark and return saved entity when it called`() {
+        every { bookmarkRepository.save(bookmark) } returns bookmark
+
+        val result = bookmarkService.saveBookmark(bookmark)
+
+        assertThat(result).isEqualTo(bookmark)
+    }
+
+    companion object {
+        val bookmark = Bookmark(
+            id = 1,
+            userId = UUID.randomUUID(),
+            surahId = 1,
+            ayahNumber = 2,
+            createdAt = Instant.now()
+        )
+    }
+}
+
