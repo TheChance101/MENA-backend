70a65fb8e33d92d2ddb9d1054fc6195f239f80c5
tareqidris <tareq.idris@prowise.com>
2025-09-16 11:44:02 +0200

    Implement get all styles endpoint

--- dukan/src/main/kotlin/net/thechance/dukan/api/controller/DukanController.kt
+++ dukan/src/main/kotlin/net/thechance/dukan/api/controller/DukanController.kt
@@ -1,13 +1,16 @@
 package net.thechance.dukan.api.controller
 
+import net.thechance.dukan.service.DukanService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class DukanController {
+@RequestMapping("/dukan")
+class DukanController(
+    private val dukanService: DukanService
+) {
+    @GetMapping("/styles")
+    fun getAllStyles () = dukanService.getAllStyles()
 
-    @GetMapping("/dukan/hello")
-    fun hello(): String {
-        return "Hello Dukan World!"
-    }
 }
---
+++ dukan/src/main/kotlin/net/thechance/dukan/service/DukanService.kt
@@ -0,0 +1,16 @@
+package net.thechance.dukan.service
+
+import net.thechance.dukan.entity.Dukan
+import net.thechance.dukan.repository.DukanCategoryRepository
+import net.thechance.dukan.repository.DukanColorRepository
+import net.thechance.dukan.repository.DukanRepository
+import org.springframework.stereotype.Service
+
+@Service
+class DukanService(
+    private val dukanRepository: DukanRepository,
+    private val dukanColorRepository: DukanColorRepository,
+    private val dukanCategoryRepository: DukanCategoryRepository
+) {
+    fun getAllStyles() = Dukan.Style.entries
+}
