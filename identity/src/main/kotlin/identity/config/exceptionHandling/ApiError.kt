ddf679faa4aaf2edc3167908c0442b028c1fdbe6
AimanYosofi <AimanYosofiUlt@gmail.com>
2025-09-16 19:48:58 +0300

    remove unrelated PR changes

--- identity/src/main/kotlin/identity/config/exceptionHandling/ApiError.kt
+++
@@ -1,9 +0,0 @@
-package identity.config.exceptionHandling
-
-data class ApiError(
-    val status: Int,
-    val exception: String,
-    val error: String,
-    val message: String?,
-    val errors: List<String>? = null
-)
