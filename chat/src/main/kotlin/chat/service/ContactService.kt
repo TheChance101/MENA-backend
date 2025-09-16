2d8668db185c7108350beadf364d5e2797d4bf8e
omer1998 <omerfaris11@gmail.com>
2025-09-16 23:47:52 +0300

    feat: implement contact service with contact model

--- chat/src/main/kotlin/chat/service/ContactService.kt
+++ chat/src/main/kotlin/chat/service/ContactService.kt
@@ -1,7 +1,19 @@
 package net.thechance.chat.service
 
+import chat.service.ContactModel
+import chat.service.toModel
+import net.thechance.chat.repository.ContactRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
-class ContactService {
+class ContactService(
+    private val contactRepository: ContactRepository
+) {
+    fun getPagedContact(pageable: Pageable): Page<ContactModel> {
+        val pagedData = contactRepository.findAll(pageable)
+        return pagedData.map { it.toModel(isMenaUser = false, imageUrl = "") }
+    }
+
 }
