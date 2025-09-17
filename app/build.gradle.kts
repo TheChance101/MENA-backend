9a73c79a242769b6f3670ae54b04e7f6264c08c8
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-09-17 16:41:09 +0300

    add dependency, plugin, and configuration for liquibase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,21 @@ plugins {
     id("io.spring.dependency-management")
     kotlin("jvm")
     kotlin("plugin.spring")
+    id("org.liquibase.gradle") version "2.2.1"
+}
+
+liquibase {
+    activities {
+        register("main") {
+            arguments = listOf(
+                "changeLogFile=src/main/resources/db/changelog/db.changelog-master.yaml",
+                "url=${project.findProperty("spring.datasource.url") as String}",
+                "username=${project.findProperty("spring.datasource.username") as String}",
+                "password=${project.findProperty("spring.datasource.password") as String}",
+            )
+        }
+    }
+    runList = "main"
 }
 
 dependencies {
@@ -21,4 +36,6 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // liquibase
+    runtimeOnly("org.liquibase:liquibase-core")
 }
