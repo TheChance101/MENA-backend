a1ae1a38d8b7b7bf7a4063302e07e6eee957ee51
ahmed samy <ahmbdelkhalek@gmail.com>
2025-09-18 23:14:20 +0300

    refactor: remove column annotation from response

--- trends/src/main/kotlin/net/thechance/trends/api/controller/ReelsController.kt
+++ trends/src/main/kotlin/net/thechance/trends/api/controller/ReelsController.kt
@@ -2,16 +2,12 @@ package net.thechance.trends.api.controller
 
 import net.thechance.trends.api.dto.PagingResponse
 import net.thechance.trends.api.dto.ReelResponse
-import net.thechance.trends.api.dto.toResponseList
+import net.thechance.trends.api.dto.toResponse
 import net.thechance.trends.service.ReelsService
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
@@ -26,7 +22,7 @@ class ReelsController(
         @AuthenticationPrincipal currentUserId: UUID
     ): ResponseEntity<PagingResponse<ReelResponse>> {
 
-        val reels = reelsService.getAllReelsByUserId(pageable, currentUserId).content.toResponseList()
+        val reels = reelsService.getAllReelsByUserId(pageable, currentUserId).content.toResponse()
 
         val result = PagingResponse(
             pageNumber = pageable.pageNumber,
--- trends/src/main/kotlin/net/thechance/trends/api/dto/ReelResponse.kt
+++ trends/src/main/kotlin/net/thechance/trends/api/dto/ReelResponse.kt
@@ -1,27 +1,18 @@
 package net.thechance.trends.api.dto
 
-import jakarta.persistence.Column
 import net.thechance.trends.entity.Category
 import net.thechance.trends.entity.Reel
 import java.time.LocalDateTime
 import java.util.*
 
 data class ReelResponse(
-    @Column(name = "reel_id")
     val reelId: UUID,
-    @Column(name = "thumbnail_url")
     val thumbnailUrl: String,
-    @Column(name = "video_url")
     val videoUrl: String,
-    @Column(name = "description")
     val description: String,
-    @Column(name = "created_at")
     val createdAt: LocalDateTime,
-    @Column(name = "likes_count")
     val likesCount: Int,
-    @Column(name = "views_count")
     val viewsCount: Int,
-    @Column(name = "categories")
     val categories: Set<Category> = emptySet()
 )
 
@@ -39,4 +30,4 @@ fun Reel.toResponse(): ReelResponse {
 }
 
 
-fun List<Reel>.toResponseList() = map { it.toResponse() }
+fun List<Reel>.toResponse() = map { it.toResponse() }
