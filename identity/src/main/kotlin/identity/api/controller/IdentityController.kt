847cf95351914f1a02b14cd123ae7f5677b31fd7
pola george <86931053+poula22@users.noreply.github.com>
2025-09-18 11:53:59 +0300

    feature: add logger to to IdentityController

--- identity/src/main/kotlin/identity/api/controller/IdentityController.kt
+++ identity/src/main/kotlin/identity/api/controller/IdentityController.kt
@@ -10,13 +10,16 @@ import net.thechance.identity.exception.InvalidCredentialsException
 import net.thechance.identity.exception.InvalidIpException
 import net.thechance.identity.exception.UserIsBlockedException
 import net.thechance.identity.service.AuthenticationService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/identity")
 class IdentityController(
-    private val authenticationService: AuthenticationService
+    private val authenticationService: AuthenticationService,
+    private val logger: Logger = LoggerFactory.getLogger(IdentityController::class.java)
 ) {
     @PostMapping("/login")
     fun login(
@@ -30,13 +33,16 @@ class IdentityController(
 
     @ExceptionHandler(AuthenticationException::class)
     fun handleAuthenticationException(exception: AuthenticationException): ResponseEntity<ErrorResponse?> {
+        logger.error("Authentication failed: ${exception.message}", exception)
         return when (exception) {
             is UserIsBlockedException -> {
                 ResponseEntity.status(403).body(ErrorResponse("User is blocked"))
             }
+
             is InvalidCredentialsException -> {
                 ResponseEntity.status(401).body(ErrorResponse("Invalid credentials"))
             }
+
             is InvalidIpException -> {
                 ResponseEntity.badRequest().body(ErrorResponse("Invalid IP"))
             }
@@ -49,6 +55,7 @@ class IdentityController(
 
     @ExceptionHandler(Exception::class)
     fun handleException(exception: Exception): ResponseEntity<ErrorResponse?> {
+        logger.error("Unknown error happened: ${exception.message}", exception)
         return ResponseEntity.internalServerError().body(ErrorResponse("unknown error happened"))
     }
 }
