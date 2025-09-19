934a0ab83394f17eaa15afaa36aa07e320398e5e
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-09-19 17:38:14 +0300

    add getPagedContact endpoint with user authentication

--- chat/src/main/kotlin/net/thechance/chat/api/controller/ContactController.kt
+++ chat/src/main/kotlin/net/thechance/chat/api/controller/ContactController.kt
@@ -20,9 +20,8 @@ class ContactController(
     @GetMapping
     fun getPagedContact(
         pageable: Pageable,
-        //@AuthenticationPrincipal userId: UUID
+        @AuthenticationPrincipal userId: UUID
     ): ResponseEntity<PagedResponse<ContactResponse>> {
-        val userId = UUID.fromString("eff6ab1b-f5a6-4d3d-ac7b-703a441275df")
         val page = contactService.getPagedContactByUserId(userId, pageable)
         return ResponseEntity.ok(page.toResponse())
     }
