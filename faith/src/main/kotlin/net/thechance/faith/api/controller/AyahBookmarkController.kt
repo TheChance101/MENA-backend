aa494b49fb56242b3234551010ec863b7acab8fc
Yusuf Nasser <yusufnassereng@gmail.com>
2025-09-19 21:09:32 +0300

    FTH-14: add endpoint to retrieve user-specific Ayah bookmarks with pagination

--- faith/src/main/kotlin/net/thechance/faith/api/controller/AyahBookmarkController.kt
+++ faith/src/main/kotlin/net/thechance/faith/api/controller/AyahBookmarkController.kt
@@ -6,12 +6,13 @@ import net.thechance.faith.api.dto.bookmark.AyahBookmarkResponse
 import net.thechance.faith.api.dto.bookmark.toBookmark
 import net.thechance.faith.api.dto.bookmark.toBookmarkResponse
 import net.thechance.faith.service.AyahBookmarkService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort.Direction
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
@@ -26,6 +27,17 @@ class AyahBookmarkController(
     ): ResponseEntity<AyahBookmarkResponse> {
         val savedBookmark = ayahBookmarkService.saveBookmark(ayahBookmarkRequest.toBookmark(userId))
         val response = savedBookmark.toBookmarkResponse()
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping("/all")
+    fun getAyahBookmarks(
+        @AuthenticationPrincipal userId: UUID,
+        @PageableDefault(size = 10, page = 1, sort = ["createdAt"], direction = Direction.DESC)
+        pageable: Pageable
+    ): ResponseEntity<Page<AyahBookmarkResponse>> {
+        val bookmarks = ayahBookmarkService.getBookmarks(userId = userId, pageable = pageable)
+        val response = bookmarks.map { it.toBookmarkResponse() }
         return ResponseEntity.ok(response)
     }
 }
