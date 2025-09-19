9f711272ec2ae2a5007c8049aeda59970ae16ca0
Yusuf Nasser <yusufnassereng@gmail.com>
2025-09-19 21:07:10 +0300

    FTH-14: add pagination support to AyahBookmarkRepository for user-specific bookmarks

--- faith/src/main/kotlin/net/thechance/faith/repository/AyahBookmarkRepository.kt
+++ faith/src/main/kotlin/net/thechance/faith/repository/AyahBookmarkRepository.kt
@@ -1,6 +1,11 @@
 package net.thechance.faith.repository
 
 import net.thechance.faith.entity.AyahBookmark
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface AyahBookmarkRepository : JpaRepository<AyahBookmark, Int>
+interface AyahBookmarkRepository : JpaRepository<AyahBookmark, Int> {
+    fun findByUserId(ownerId: UUID, pageable: Pageable): Page<AyahBookmark>
+}
