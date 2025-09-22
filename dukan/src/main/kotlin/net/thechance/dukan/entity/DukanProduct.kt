b0a0173517b9b483b82e4329742125b839fb6c1f
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-09-22 09:44:17 +0300

    create DukanProduct entity

---
+++ dukan/src/main/kotlin/net/thechance/dukan/entity/DukanProduct.kt
@@ -0,0 +1,42 @@
+package net.thechance.dukan.entity
+
+import jakarta.persistence.*
+import java.util.*
+
+@Table(
+    name = "dukan_products",
+    schema = "dukan",
+    uniqueConstraints = [UniqueConstraint(columnNames = ["dukan_id", "name"])]
+)
+@Entity
+data class DukanProduct(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID = UUID.randomUUID(),
+
+    @Column(name = "name", nullable = false)
+    val name: String,
+
+    @ManyToOne
+    @JoinColumn(name = "shelf_id", nullable = false)
+    val shelf: DukanShlef,
+
+    @ManyToOne
+    @JoinColumn(name = "dukan_id", nullable = false)
+    val dukan: Dukan,
+
+    @Column(name = "price", nullable = false)
+    val price: Double,
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    val description: String,
+
+    @ElementCollection
+    @CollectionTable(
+        name = "product_images",
+        schema = "dukan",
+        joinColumns = [JoinColumn(name = "product_id")]
+    )
+    @Column(name = "image_url", nullable = false)
+    val imageUrls: List<String>
+)
