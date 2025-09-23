d1532c40e9f2d4bc010418bea0a544bc0423c149
Malak Raef <malak.raef20@gmail.com>
2025-09-23 04:47:15 +0300

    feat: create TransactionController to make the endpoint for getting transactions

---
+++ wallet/src/main/kotlin/net/thechance/wallet/api/controller/TransactionController.kt
@@ -0,0 +1,47 @@
+package net.thechance.wallet.api.controller
+
+import net.thechance.wallet.api.dto.transaction.TransactionFilterRequest
+import net.thechance.wallet.api.dto.transaction.TransactionResponse
+import net.thechance.wallet.api.dto.transaction.TransactionType
+import net.thechance.wallet.api.dto.transaction.toResponse
+import net.thechance.wallet.entity.Transaction
+import net.thechance.wallet.service.TransactionService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.util.*
+
+
+@RestController
+@RequestMapping("/wallet")
+class TransactionController(
+    private val transactionService: TransactionService
+) {
+    @GetMapping("/transactions")
+    fun getFilteredTransactions(
+        @AuthenticationPrincipal userId: UUID,
+        @RequestParam(required = false) type: TransactionType,
+        @RequestParam(required = false) status: Transaction.Status?,
+        @RequestParam(required = false) startDate: LocalDateTime?,
+        @RequestParam(required = false) endDate: LocalDateTime?,
+        pageable: Pageable
+    ): ResponseEntity<Page<TransactionResponse>> {
+        val filter = TransactionFilterRequest(
+            type = type,
+            status = status,
+            startDate = startDate,
+            endDate = endDate
+        )
+        val result =
+            transactionService.getFilteredTransactions(filter = filter, pageable = pageable, currentUserId = userId)
+                .map { it.toResponse() }
+
+        return ResponseEntity.ok(result)
+    }
+}
