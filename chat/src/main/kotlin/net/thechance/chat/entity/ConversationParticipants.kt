1c58a09cb010a4e2ad2a2fb6e65f498afa3665b1
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-09-23 22:20:31 +0300

    update entity classes

--- chat/src/main/kotlin/net/thechance/chat/entity/ConversationParticipants.kt
+++ chat/src/main/kotlin/net/thechance/chat/entity/ConversationParticipants.kt
@@ -1,21 +1,20 @@
 package net.thechance.chat.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
-import jakarta.persistence.UniqueConstraint
+import jakarta.persistence.*
 import java.util.UUID
 
 @Entity
-@Table(name = "conversation_participant", schema = "chat", uniqueConstraints = [
-    UniqueConstraint(columnNames = ["conversation_id","user_id"])
-])
-data class ConversationParticipants (
-    @Id @Column(columnDefinition = "uuid", nullable = false, updatable = false)
-    private val id: UUID= UUID.randomUUID(),
+@Table(name = "conversation_participant", schema = "chat")
+data class ConversationParticipants(
+    @EmbeddedId
+    val id: ConversationParticipantsId,
+)
+
+@Embeddable
+data class ConversationParticipantsId(
     @Column(name = "conversation_id", nullable = false)
-    private val conversationId : UUID,
+    val conversationId: UUID,
+
     @Column(name = "user_id", nullable = false)
-    private val userId: UUID
+    val userId: UUID,
 )
--- chat/src/main/kotlin/net/thechance/chat/entity/MessageSeen.kt
+++ chat/src/main/kotlin/net/thechance/chat/entity/MessageSeen.kt
@@ -1,18 +1,24 @@
 package net.thechance.chat.entity
 
 import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import jakarta.persistence.EmbeddedId
 import jakarta.persistence.Entity
-import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.util.UUID
 
 @Entity
 @Table(name = "message_seen", schema = "chat")
 data class MessageSeen(
-    @Id @Column(columnDefinition = "uuid", updatable = false, nullable = false)
-    val id: UUID = UUID.randomUUID(),
-    @Column(name= "user_id", nullable = false)
+    @EmbeddedId
+    val id: MessageSeenId,
+)
+
+@Embeddable
+data class MessageSeenId(
+    @Column(name = "user_id", nullable = false)
     val userId: UUID,
+
     @Column(name = "message_id", nullable = false)
-    val messageId : UUID
+    val messageId: UUID,
 )
