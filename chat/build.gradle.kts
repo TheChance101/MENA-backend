0e438a6ebb41c4b837e1a060c3d196f306ec42a5
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-09-24 19:58:32 +0300

    exclude service model packages from build; refine project structure

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -38,7 +38,7 @@ kover.reports {
         excludes {
             packages("net.thechance.chat.api.dto*")
             packages("net.thechance.chat.entity*")
-            packages("net.thechance.chat.repository*")
+            packages("net.thechance.chat.service.model*")
         }
     }
 }
