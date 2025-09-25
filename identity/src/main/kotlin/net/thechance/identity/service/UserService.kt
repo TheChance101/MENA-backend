2fd87102ab40423e88e77716680ebdac760f672b
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-09-25 16:43:31 +0100

    feature: add UserNotFoundException and findByUsername method to UserService

--- identity/src/main/kotlin/net/thechance/identity/service/UserService.kt
+++ identity/src/main/kotlin/net/thechance/identity/service/UserService.kt
@@ -1,21 +1,29 @@
 package net.thechance.identity.service
 
+import net.thechance.identity.api.dto.ProfileResponse
 import net.thechance.identity.entity.User
 import net.thechance.identity.exception.InvalidCredentialsException
+import net.thechance.identity.exception.UserNotFoundException
+import net.thechance.identity.mapper.toResponse
 import net.thechance.identity.repository.UserRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class UserService(
-    val userRepository: UserRepository
+	val userRepository: UserRepository
 ) {
 
-    fun findByPhoneNumber(phoneNumber: String): User {
-        return userRepository.findByPhoneNumber(phoneNumber) ?: throw InvalidCredentialsException("User not found")
-    }
+	fun findByPhoneNumber(phoneNumber: String): User {
+		return userRepository.findByPhoneNumber(phoneNumber) ?: throw InvalidCredentialsException("User not found")
+	}
 
-    fun userExists(userId: UUID): Boolean {
-        return userRepository.existsById(userId)
-    }
+	fun findByUsername(username: String): ProfileResponse {
+		return userRepository.findByUsername(username)?.toResponse()
+			?: throw UserNotFoundException("User with username: $username not found")
+	}
+
+	fun userExists(userId: UUID): Boolean {
+		return userRepository.existsById(userId)
+	}
 }
