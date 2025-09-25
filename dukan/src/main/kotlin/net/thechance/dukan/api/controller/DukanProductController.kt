6521d94e753ad7b176f96e160b60ed023c52fd99
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-09-25 13:26:28 +0300

    create dukan product controller with get mapping by shelf id

---
+++ dukan/src/main/kotlin/net/thechance/dukan/api/controller/DukanProductController.kt
@@ -0,0 +1,32 @@
+package net.thechance.dukan.api.controller
+
+import net.thechance.dukan.api.dto.DukanProductResponse
+import net.thechance.dukan.api.dto.toProductResponse
+import net.thechance.dukan.entity.DukanProduct
+import net.thechance.dukan.service.DukanProductService
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/products")
+class DukanProductController(
+    private val productService: DukanProductService
+) {
+    @GetMapping("/shelf/{shelfId}")
+    fun getProductsByShelf(
+        @PathVariable shelfId: UUID,
+        @PageableDefault(size = 10, page = 0)
+        pageable: Pageable
+    ): ResponseEntity<Page<DukanProductResponse>> {
+//        return productService.getProductsByShelf(shelfId,pageable)
+        val products = productService.getProductsByShelf(shelfId,pageable)
+        val productsResponse = products.map { it.toProductResponse() }
+        return ResponseEntity.ok(productsResponse)
+    }
+}
