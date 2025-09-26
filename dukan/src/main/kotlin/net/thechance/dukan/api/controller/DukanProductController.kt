eb79e43b3a0514eacc8dddcbcf8bcdb7512dca8f
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-09-26 21:30:58 +0300

    rename products endpoint resource to product

--- dukan/src/main/kotlin/net/thechance/dukan/api/controller/DukanProductController.kt
+++ dukan/src/main/kotlin/net/thechance/dukan/api/controller/DukanProductController.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/product")
 class DukanProductController(
     private val productService: DukanProductService
 ) {
