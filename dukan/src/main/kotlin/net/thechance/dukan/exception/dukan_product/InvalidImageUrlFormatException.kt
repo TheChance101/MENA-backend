f2d118d5babd7e891f377919347ffaae57c4e96f
tareqidris <tareq.idris@prowise.com>
2025-09-26 19:26:27 +0200

    Revert "Reduce conflict possibility & check image validity before upload"
    This reverts commit a23f449f69482739064c5e755349cf96ea2f910c.

--- dukan/src/main/kotlin/net/thechance/dukan/exception/dukan_product/InvalidImageUrlFormatException.kt
+++
@@ -1,3 +0,0 @@
-package net.thechance.dukan.exception.dukan_product
-
-class InvalidImageUrlFormatException() : Exception("Invalid image url format")
--- dukan/src/main/kotlin/net/thechance/dukan/service/DukanProductService.kt
+++ dukan/src/main/kotlin/net/thechance/dukan/service/DukanProductService.kt
@@ -1,12 +1,10 @@
 package net.thechance.dukan.service
 
 import jakarta.transaction.Transactional
-import net.thechance.dukan.exception.dukan_product.InvalidImageUrlFormatException
 import net.thechance.dukan.exception.dukan_product.ProductNotFoundException
 import net.thechance.dukan.repository.DukanProductRepository
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
-import java.time.Instant
 import java.util.UUID
 
 @Service
@@ -21,26 +19,16 @@ class DukanProductService(
         files.forEach { file ->
             val imageUrl = imageStorageService.uploadImage(
                 file = file,
-                fileName = "${product.name}-${Instant.now().toEpochMilli()}-${file.originalFilename}",
-                folderName = PRODUCT_FOLDER_NAME
+                fileName = "${product.name}-${file.originalFilename}",
+                folderName = PRODCUT_FOLDER_NAME
             )
             imageUrls.add(imageUrl)
         }
-        checkImageValidity(imageUrls)
         dukanProductRepository.save(product.copy(imageUrls = imageUrls))
         return imageUrls
     }
 
-    private fun checkImageValidity(imageUrls: List<String>) {
-        imageUrls.forEach { url ->
-            if (!url.matches(IMAGE_URL_PATTERN.toRegex())) {
-                throw InvalidImageUrlFormatException()
-            }
-        }
-    }
-
     companion object {
-        private const val PRODUCT_FOLDER_NAME = "product"
-        private const val IMAGE_URL_PATTERN = "^[a-zA-Z0-9/_.-]+$"
+        private val PRODCUT_FOLDER_NAME = "product"
     }
 }
