8d2831531f2ebe5c48cd2370c1c956574770bc41
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-09-27 02:42:21 +0300

    refactor: move markChatMessagesAsRead method to messagingService and clean up MessageRepository

--- chat/src/main/kotlin/net/thechance/chat/entity/GroupChat.kt
+++ chat/src/main/kotlin/net/thechance/chat/entity/GroupChat.kt
@@ -1,6 +1,12 @@
 package net.thechance.chat.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.ForeignKey
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "group_chats", schema = "chat")
--- chat/src/main/kotlin/net/thechance/chat/repository/MessageRepository.kt
+++ chat/src/main/kotlin/net/thechance/chat/repository/MessageRepository.kt
@@ -4,8 +4,6 @@ import net.thechance.chat.entity.ContactUser
 import net.thechance.chat.entity.Message
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
 import java.time.Instant
 import java.util.UUID
 
@@ -20,21 +18,4 @@ interface MessageRepository : JpaRepository<Message, UUID> {
     fun getAllByChatIdAndSendAtAfterOrderBySendAtAsc(chatId: UUID, sendAt: Instant, pageable: Pageable): List<Message>
 
     fun findAllByChatIdAndReadByUsersNotContaining(chatId: UUID, user: ContactUser, pageable: Pageable): List<Message>
-
-    fun markChatMessagesAsRead(chatId: UUID, user: ContactUser) {
-        var page = 0
-        val pageSize = PAGE_SIZE
-        var messages: List<Message>
-        do {
-            messages = findAllByChatIdAndReadByUsersNotContaining(chatId, user, Pageable.ofSize(pageSize).withPage(page))
-            if (messages.isNotEmpty()) {
-                saveAll(messages.onEach { it.readByUsers += user })
-            }
-            page++
-        } while (messages.size == pageSize)
-    }
-
-    companion object {
-        const val PAGE_SIZE = 500
-    }
 }
