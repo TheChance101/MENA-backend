5d30d160710d236c78b1a11627950fb78cf98f12
raouf-kamel <raoufkamel2013@gmail.com>
2025-09-27 17:30:33 +0300

    add endpoint get or create conversation

--- chat/src/main/kotlin/net/thechance/chat/api/controller/ChatController.kt
+++ chat/src/main/kotlin/net/thechance/chat/api/controller/ChatController.kt
@@ -1,13 +1,20 @@
 package net.thechance.chat.api.controller
 
 import net.thechance.chat.service.ChatService
+import net.thechance.chat.service.model.ChatModel
 import net.thechance.chat.service.model.MessageModel
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import java.util.*
 
+@RequestMapping("/chat")
 @Controller
 class ChatController(
     private val messagingTemplate: SimpMessagingTemplate,
@@ -36,6 +43,15 @@ class ChatController(
     fun addUser(@Payload chatMessage: MessageModel): MessageModel {
         chatService.saveMessage(chatMessage)
         return chatMessage
+    }
+
+    @GetMapping
+    @ResponseBody
+    fun getOrCreateConversation(
+        @RequestParam requesterId: UUID,
+        @RequestParam otherUserId: UUID
+    ): ChatModel {
+        return chatService.getOrCreateConversationByParticipants(requesterId, otherUserId)
     }
 
 }
