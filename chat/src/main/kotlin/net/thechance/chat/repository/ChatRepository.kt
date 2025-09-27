c8ad9b9a888c9e53479a9fc1f2da554ff0046b02
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-09-27 20:36:58 +0300

    refactor: rename method for consistency in ChatRepository and ChatService

--- chat/src/main/kotlin/net/thechance/chat/repository/ChatRepository.kt
+++ chat/src/main/kotlin/net/thechance/chat/repository/ChatRepository.kt
@@ -2,12 +2,10 @@ package net.thechance.chat.repository
 
 import net.thechance.chat.entity.Chat
 import net.thechance.chat.entity.ContactUser
-import net.thechance.chat.service.model.ChatModel
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import java.util.UUID
 
 interface ChatRepository : JpaRepository<Chat, UUID> {
     fun findByIdIs(id: UUID): Chat?
-    fun findByUsersIsAndGroupChatIsNull(users: Set<ContactUser>): Chat?
+    fun findByUsersAndGroupChatIsNull(users: Set<ContactUser>): Chat?
 }
