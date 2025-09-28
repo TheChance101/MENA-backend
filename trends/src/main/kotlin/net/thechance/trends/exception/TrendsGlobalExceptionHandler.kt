39cddb98ef5a2472407691c6424301f154637c7d
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-09-28 19:53:03 +0300

    refactor: change packages to match module code style

--- trends/src/main/kotlin/net/thechance/trends/exception/TrendsGlobalExceptionHandler.kt
+++ trends/src/main/kotlin/net/thechance/trends/exception/TrendsGlobalExceptionHandler.kt
@@ -1,9 +1,5 @@
-package net.thechance.trends.api.controller
+package net.thechance.trends.exception
 
-import net.thechance.trends.exception.InvalidTrendInputException
-import net.thechance.trends.exception.ReelNotFoundException
-import net.thechance.trends.exception.TrendCategoryNotFoundException
-import net.thechance.trends.exception.TrendUserNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
