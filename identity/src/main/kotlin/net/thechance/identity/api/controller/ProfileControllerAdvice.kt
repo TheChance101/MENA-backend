5d463fac1542aae2d3c5f90cd05606442ee015bd
pola george <86931053+poula22@users.noreply.github.com>
2025-10-01 21:15:30 +0300

    feature: handle InvalidImageException and UnknownErrorException in ProfileControllerAdvice

--- identity/src/main/kotlin/net/thechance/identity/api/controller/ProfileControllerAdvice.kt
+++ identity/src/main/kotlin/net/thechance/identity/api/controller/ProfileControllerAdvice.kt
@@ -1,6 +1,8 @@
 package net.thechance.identity.api.controller
 
 import net.thechance.identity.api.dto.ErrorResponse
+import net.thechance.identity.exception.InvalidImageException
+import net.thechance.identity.exception.UnknownErrorException
 import net.thechance.identity.exception.UserNotFoundException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -23,4 +25,19 @@ class ProfileControllerAdvice {
             .body(ErrorResponse("User not found"))
     }
 
+    @ExceptionHandler(InvalidImageException::class)
+    fun handleInvalidImageException(exception: InvalidImageException): ResponseEntity<ErrorResponse?> {
+        logger.error("Invalid image: ${exception.message}", exception)
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse("Image extesion ${exception.extension} is not supported"))
+    }
+
+    @ExceptionHandler(UnknownErrorException::class)
+    fun handleException(exception: Exception): ResponseEntity<ErrorResponse?> {
+        logger.error(exception.message, exception)
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ErrorResponse("Internal server error"))
+    }
 }
--- identity/src/main/kotlin/net/thechance/identity/exception/UploadImageException.kt
+++ identity/src/main/kotlin/net/thechance/identity/exception/UploadImageException.kt
@@ -3,7 +3,7 @@ package net.thechance.identity.exception
 abstract class UploadImageException(message: String): Exception(message)
 
 class InvalidImageException(
-    extension: String
+    val extension: String
 ): UploadImageException("uploading image with invalid extension:  $extension")
 
 class UnknownErrorException(message: String): UploadImageException(message)
