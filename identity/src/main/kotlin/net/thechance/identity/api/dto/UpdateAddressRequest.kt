cd4db5df3886d1f0458f4dd10c8de14b14cf8273
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-10-08 18:53:09 +0300

    feature: rename update and create address requests.

--- identity/src/main/kotlin/net/thechance/identity/api/dto/UpdateAddressRequest.kt
+++ identity/src/main/kotlin/net/thechance/identity/api/dto/UpdateAddressRequest.kt
@@ -4,18 +4,20 @@ import jakarta.validation.constraints.DecimalMax
 import jakarta.validation.constraints.DecimalMin
 import jakarta.validation.constraints.NotBlank
 
-data class AddAddressRequest(
+data class UpdateAddressRequest(
     @field:DecimalMin(value = "-90.0", message = "Latitude must be at least -90.")
     @field:DecimalMax(value = "90.0", message = "Latitude must not be greater than 90.")
-    val latitude: Double,
+    val latitude: Double?,
 
     @field:DecimalMin(value = "-180.0", message = "Longitude must be at least -180.")
     @field:DecimalMax(value = "180.0", message = "Longitude must not be greater than 180.")
-    val longitude: Double,
+    val longitude: Double?,
 
     @field:NotBlank(message = "AddressLine must not be empty.")
-    val addressLine: String,
+    val addressLine: String?,
 
     @field:NotBlank(message = "AddressType must not be empty.")
-    val addressType: String,
+    val addressType: String?,
+
+    val isActive: Boolean?
 )
