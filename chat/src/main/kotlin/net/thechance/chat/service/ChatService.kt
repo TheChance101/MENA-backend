4edf114fa3619e426f12cbda762dd1df0f9a70d8
raouf-kamel <raoufkamel2013@gmail.com>
2025-10-09 01:29:21 +0300

    add getUserChats

--- chat/src/main/kotlin/net/thechance/chat/service/ChatService.kt
+++ chat/src/main/kotlin/net/thechance/chat/service/ChatService.kt
@@ -1,11 +1,15 @@
 package net.thechance.chat.service
 
 import jakarta.persistence.EntityManager
+import net.thechance.chat.api.dto.ChatsResponse
+import net.thechance.chat.api.dto.toResponse
 import net.thechance.chat.entity.Chat
 import net.thechance.chat.entity.Message
 import net.thechance.chat.repository.ChatRepository
 import net.thechance.chat.repository.MessageRepository
 import net.thechance.chat.service.args.CreateMessageArgs
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,7 +20,8 @@ class ChatService(
     private val messageRepository: MessageRepository,
     private val chatRepository: ChatRepository,
     private val contactUserService: ContactUserService,
-    private val entityManager: EntityManager
+    private val entityManager: EntityManager,
+    private val contactService: ContactService,
 ) {
     @Transactional
     fun getOrCreateConversationByParticipants(userId: UUID, receiverId: UUID): Chat {
@@ -50,4 +55,24 @@ class ChatService(
 
     fun markChatMessagesAsRead(chatId: UUID, userId: UUID) =
         messageRepository.updateIsReadByChatIdAndSenderIdNot(chatId = chatId, userId = userId)
+
+    fun getUserChats(userId: UUID, pageable: Pageable): Page<ChatsResponse> {
+        val chats = chatRepository.findAllByUserId(userId, pageable)
+        val chatResponses = chats.map { chat ->
+            val lastMessage = messageRepository.findTopByChatIdOrderBySentAtDesc(chat.id)
+            val unreadCount = messageRepository.countByChatIdAndSenderIdNotAndIsReadFalse(chat.id, userId)
+            val contact = chat.users.firstOrNull { it.id != userId }?.let { requester ->
+                contactService.getContactByOwnerIdAndContactUserId(userId, requester.id)
+            }
+
+            chat.toResponse(
+                requesterId = userId,
+                contact = contact,
+                message = lastMessage,
+                unreadCount = unreadCount
+            )
+        }
+        return PageImpl(chatResponses, pageable, chatRepository.countByUserId(userId))
+
+    }
 }
