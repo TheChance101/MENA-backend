b55ec3541782d299dd90342286b512ddc67ba62f
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-10-08 23:37:54 +0300

    feature: update address entity.

--- identity/src/main/kotlin/net/thechance/identity/entity/Address.kt
+++ identity/src/main/kotlin/net/thechance/identity/entity/Address.kt
@@ -8,26 +8,26 @@ import java.util.*
 @Table(name = "Addresses", schema = "identity")
 class Address(
     @Id
-    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    @Column(name = "uuid", updatable = false, nullable = false)
     val id: UUID = UUID.randomUUID(),
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false, updatable = false)
     val user: User,
 
-    @Column(columnDefinition = "latitude", nullable = false)
+    @Column(name = "latitude", nullable = false)
     val latitude: Double,
 
-    @Column(columnDefinition = "longitude", nullable = false)
+    @Column(name = "longitude", nullable = false)
     val longitude: Double,
 
-    @Column(columnDefinition = "address_line", nullable = false)
+    @Column(name = "address_line", nullable = false)
     val addressLine: String,
 
-    @Column(columnDefinition = "address_type", nullable = false)
+    @Column(name = "address_type", nullable = false)
     val addressType: String,
 
-    @Column(columnDefinition = "is_active", nullable = false)
+    @Column(name = "is_active", nullable = false)
     val isActive: Boolean = false,
 
     @Column(name = "created_at", nullable = false)
