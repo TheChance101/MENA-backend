a43277476e15d770dff1f8213ada474ba27c428e
raouf-kamel <raoufkamel2013@gmail.com>
2025-10-09 01:29:07 +0300

    add findAllByUserId ,countByUserId

--- chat/src/main/kotlin/net/thechance/chat/repository/ChatRepository.kt
+++ chat/src/main/kotlin/net/thechance/chat/repository/ChatRepository.kt
@@ -4,6 +4,7 @@ import net.thechance.chat.entity.Chat
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.util.UUID
+import org.springframework.data.domain.Pageable
 
 interface ChatRepository : JpaRepository<Chat, UUID> {
     @Query(
@@ -17,5 +18,11 @@ interface ChatRepository : JpaRepository<Chat, UUID> {
         """
     )
     fun findByUsersIds(userIds: Set<UUID>): Chat?
+
+    @Query("SELECT c FROM Chat c JOIN c.users u WHERE u.id = :userId")
+    fun findAllByUserId(userId: UUID, pageable: Pageable): List<Chat>
+    @Query("SELECT COUNT(c) FROM Chat c JOIN c.users u WHERE u.id = :userId")
+    fun countByUserId(userId: UUID): Long
+
 }
 
