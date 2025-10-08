99c52bfe7ffd654031bfccc827ba80513a4639da
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-10-08 17:21:33 +0300

    feature: add getAllReels query in repository

--- trends/src/main/kotlin/net/thechance/trends/repository/ReelsRepository.kt
+++ trends/src/main/kotlin/net/thechance/trends/repository/ReelsRepository.kt
@@ -1,13 +1,30 @@
 package net.thechance.trends.repository
 
+import net.thechance.trends.entity.Category
 import net.thechance.trends.entity.Reel
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface ReelsRepository : JpaRepository<Reel, UUID> {
     fun findByOwnerId(ownerId: UUID, pageable: Pageable): Page<Reel>
     fun findByIdAndOwnerId(id: UUID, ownerId: UUID): Reel?
     fun existsByIdAndOwnerId(id: UUID, ownerId: UUID): Boolean
+
+    @Query("""
+    SELECT DISTINCT r FROM Reel r
+    JOIN FETCH r.categories rc
+    WHERE r.isPublished = true
+    AND rc.id IN (
+        SELECT uc.id FROM TrendUser u
+        JOIN u.categories uc
+        WHERE u.userId = :userId
+        )
+    """)
+    fun getReelFeedForUser(
+        userId: UUID,
+        pageable: Pageable
+    ): Page<Reel>
 }
