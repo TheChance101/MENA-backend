974d30846b0a2d0994d68732bf2b649e8c6cc840
ahmed fikry <ahmedfikryelshimi24@gmail.com>
2025-10-10 19:09:46 +0300

    feat: add MessageImagesRequestDto for image handling

--- chat/src/main/kotlin/net/thechance/chat/service/args/CreateMessageArgs.kt
+++ chat/src/main/kotlin/net/thechance/chat/service/args/CreateMessageArgs.kt
@@ -1,6 +1,5 @@
 package net.thechance.chat.service.args
 
-import org.springframework.web.multipart.MultipartFile
 import java.time.Instant
 import java.util.*
 
@@ -9,7 +8,6 @@ data class CreateMessageArgs(
     val senderId: UUID,
     val chatId: UUID,
     val text: String?,
-    val images: List<MultipartFile>?,
     val sendAt: Instant,
     val isRead: Boolean
 )
