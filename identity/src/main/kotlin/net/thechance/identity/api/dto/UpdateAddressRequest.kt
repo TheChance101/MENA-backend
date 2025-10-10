1811839ee9c234883d7cfb403131706e7e108ed9
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-10-10 23:26:47 +0300

    feature: resolve problem of check the blank if and only if there is a value and not null.

--- identity/src/main/kotlin/net/thechance/identity/api/dto/UpdateAddressRequest.kt
+++ identity/src/main/kotlin/net/thechance/identity/api/dto/UpdateAddressRequest.kt
@@ -3,6 +3,7 @@ package net.thechance.identity.api.dto
 import jakarta.validation.constraints.DecimalMax
 import jakarta.validation.constraints.DecimalMin
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 
 data class UpdateAddressRequest(
     @field:DecimalMin(value = "-90.0", message = "Latitude must be at least -90.")
@@ -13,10 +14,16 @@ data class UpdateAddressRequest(
     @field:DecimalMax(value = "180.0", message = "Longitude must not be greater than 180.")
     val longitude: Double?,
 
-    @field:NotBlank(message = "AddressLine must not be empty.")
+    @field:Pattern(
+        regexp = ".*\\S.*",
+        message = "AddressLine must not be empty."
+    )
     val addressLine: String?,
 
-    @field:NotBlank(message = "AddressType must not be empty.")
+    @field:Pattern(
+        regexp = ".*\\S.*",
+        message = "AddressType must not be empty."
+    )
     val addressType: String?,
 
     val isActive: Boolean?
