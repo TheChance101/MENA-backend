f9856ecfce402fdb6c396b41ddf40c74a2a7f5ac
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-10-11 17:23:49 +0300

    refactor: add create to paging response to return the correct page number

--- trends/src/main/kotlin/net/thechance/trends/api/dto/PagingResponse.kt
+++ trends/src/main/kotlin/net/thechance/trends/api/dto/PagingResponse.kt
@@ -4,4 +4,18 @@ data class PagingResponse<T>(
     val pageNumber: Int,
     val results: List<T>,
     val totalResults: Int
-)
+) {
+    companion object {
+        fun <T> create(
+            pageNumber: Int,
+            results: List<T>,
+            totalResults: Int
+        ): PagingResponse<T> {
+            return PagingResponse(
+                pageNumber = maxOf(1, pageNumber),
+                results = results,
+                totalResults = totalResults
+            )
+        }
+    }
+}
