8570cdb1abbafcf33dd81b86457160079bfd3ebc
Yusuf Nasser <yusufnassereng@gmail.com>
2025-10-13 16:10:40 +0300

    TRN-56: add validation to PatchUserCategoriesRequest

--- trends/src/main/kotlin/net/thechance/trends/api/dto/category/PatchUserCategoriesRequest.kt
+++ trends/src/main/kotlin/net/thechance/trends/api/dto/category/PatchUserCategoriesRequest.kt
@@ -1,8 +1,15 @@
 package net.thechance.trends.api.dto.category
 
+import jakarta.validation.constraints.AssertTrue
 import java.util.*
 
 data class PatchUserCategoriesRequest(
     val add: List<UUID> = emptyList(),
     val remove: List<UUID> = emptyList()
-)
+) {
+    @AssertTrue(message = "At least one operation (add or remove) is required")
+    fun isAtLeastOneOperationPresent() = add.isNotEmpty() || remove.isNotEmpty()
+
+    @AssertTrue(message = "Cannot add and remove the same category")
+    fun hasNoOverlap() = add.intersect(remove.toSet()).isEmpty()
+}
