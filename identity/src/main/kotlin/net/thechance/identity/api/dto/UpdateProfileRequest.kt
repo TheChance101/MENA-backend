1e2574cccacc64c20f6c2f90834faa2f42e27655
pola george <86931053+poula22@users.noreply.github.com>
2025-10-17 13:55:32 +0300

    refactor: simplify UserService and ProfileController, improve validation
    - Removed `dateFormatter` utility and replaced `birthDate` formatting with `toString`.
    - Introduced `UserServiceModel` to decouple `UserService` from API layer.
    - Updated `UserService.updateUserProfile` with enhanced parameters and logic.
    - Enhanced validation in `UpdateProfileRequest`
    - Updated `ProfileController` to use `UserServiceModel` and support optional image updates.

--- identity/src/main/kotlin/net/thechance/identity/api/dto/UpdateProfileRequest.kt
+++ identity/src/main/kotlin/net/thechance/identity/api/dto/UpdateProfileRequest.kt
@@ -2,14 +2,36 @@ package net.thechance.identity.api.dto
 
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import net.thechance.identity.service.model.UserServiceModel
+import org.hibernate.validator.constraints.Length
+import java.util.*
 
 data class UpdateProfileRequest (
+    @field:NotBlank(message = "username must not be blank")
+    @field:Length(min = 3, message = "username must be at least 3 characters")
     val username: String,
+    @field:NotBlank(message = "firstName must not be blank")
+    @field:Length(min = 3, message = "username must be at least 3 characters")
     val firstName: String,
+    @field:NotBlank(message = "lastName must not be blank")
     val lastName: String,
-    val imageUrl: String?,
+    @field:NotBlank(message = "birthDate must not be blank")
     val birthDate: String,
     @field:Min(1, message = "Gender must be 1 or 2")
     @field:Max(2, message = "Gender must be 1 or 2")
     val gender: Int,
+    val imageUrl: String?,
+    val updateImage: Boolean = false
+)
+
+fun UpdateProfileRequest.toUserServiceModel(id: UUID) = UserServiceModel(
+    id = id,
+    username = username,
+    firstName = firstName,
+    lastName = lastName,
+    birthDate = birthDate,
+    gender = gender,
+    imageUrl = imageUrl,
+    phoneNumber = ""
 )
--- identity/src/main/kotlin/net/thechance/identity/mapper/util/dateFormmater.kt
+++
@@ -1,9 +0,0 @@
-package net.thechance.identity.mapper.util
-
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-
-const val DEFAULT_DATE_FORMAT = "yyyy-MM-dd"
-fun LocalDate.formatAsString(): String {
-    return format(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT))
-}
