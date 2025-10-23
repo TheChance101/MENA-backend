a189be4a7d7d14573acee79898b8b5ad15b89292
edrees <muhammed.edrees101@gmail.com>
2025-10-23 20:13:25 +0300

    refactor: add InitiateTransactionEventMapper for transaction parameter mapping

--- wallet/src/main/kotlin/net/thechance/wallet/eventListener/WalletEventListener.kt
+++ wallet/src/main/kotlin/net/thechance/wallet/eventListener/WalletEventListener.kt
@@ -1,6 +1,7 @@
 package net.thechance.wallet.eventListener
 
 import net.thechance.events.wallet.InitiateTransactionEvent
+import net.thechance.wallet.eventListener.mapper.toInitiateTransactionParams
 import net.thechance.wallet.service.TransactionService
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
