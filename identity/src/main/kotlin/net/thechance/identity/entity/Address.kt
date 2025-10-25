1fc964da93a70d4de185cfbaef60896ceb48b437
Thoraya <thoriahamdy99@gmail.com>
2025-10-25 17:42:33 +0300

    refactor: make the address data class as it is not has any relation.

--- identity/src/main/kotlin/net/thechance/identity/entity/Address.kt
+++ identity/src/main/kotlin/net/thechance/identity/entity/Address.kt
@@ -9,7 +9,7 @@ import java.util.*
 
 @Entity
 @Table(name = "Addresses", schema = "identity")
-class Address(
+data class Address(
     @Id
     @Column(name = "uuid", updatable = false, nullable = false)
     val id: UUID = UUID.randomUUID(),
@@ -38,27 +38,3 @@ class Address(
     @Column(name = "updated_at", nullable = false)
     val updatedAt: Instant = Instant.now(),
 )
-
-fun Address.copy(
-    id: UUID = this.id,
-    userId: UUID = this.userId,
-    latitude: Double = this.latitude,
-    longitude: Double = this.longitude,
-    addressLine: String = this.addressLine,
-    addressType: String = this.addressType,
-    isActive: Boolean = this.isActive,
-    createdAt: Instant = this.createdAt,
-    updatedAt: Instant = this.updatedAt,
-): Address {
-    return Address(
-        id = id,
-        userId = userId,
-        latitude = latitude,
-        longitude = longitude,
-        addressLine = addressLine,
-        addressType = addressType,
-        isActive = isActive,
-        createdAt = createdAt,
-        updatedAt = updatedAt
-    )
-}
