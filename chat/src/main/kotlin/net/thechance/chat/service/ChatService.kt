ae73276fc37e74d3cac33377cf1521bb705c76a7
bilalazzam <blalazam065@gmail.com>
2025-10-25 06:18:42 +0300

    rename images to timestamp

--- chat/src/main/kotlin/net/thechance/chat/service/ChatService.kt
+++ chat/src/main/kotlin/net/thechance/chat/service/ChatService.kt
@@ -53,7 +53,7 @@ class ChatService(
     fun saveMessageImage(args: MessageImageRequestArgs): Message {
         val imageUrl = attachmentStorageService.uploadImage(
             file = args.image,
-            fileName = args.image.originalFilename ?: "${UUID.randomUUID()}-Untitled",
+            fileName = args.image.originalFilename ?: "${Instant.now()}-Untitled",
             folderName = FOLDER_NAME
         )
 
