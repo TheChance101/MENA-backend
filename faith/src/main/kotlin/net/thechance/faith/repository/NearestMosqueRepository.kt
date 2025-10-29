a30e98b66b711044459271d5e8bb54e94890833f
Hasan Ali <ihasanalihasan@gmail.com>
2025-10-29 14:07:09 +0300

    FTH-104: create NearestMosqueService for managing mosque creation and image uploads

--- faith/src/main/kotlin/net/thechance/faith/repository/NearestMosqueRepository.kt
+++ faith/src/main/kotlin/net/thechance/faith/repository/NearestMosqueRepository.kt
@@ -10,29 +10,6 @@ import java.util.*
 @Repository
 interface NearestMosqueRepository: JpaRepository<Mosque, UUID> {
 
-    @Query("""
-    SELECT *, 
-        (6371 * acos(
-            cos(radians(:latitude)) * cos(radians(latitude)) *
-            cos(radians(longitude) - radians(:longitude)) +
-            sin(radians(:latitude)) * sin(radians(latitude))
-        )) AS distance
-    FROM faith.mosque
-    WHERE 
-        (6371 * acos(
-            cos(radians(:latitude)) * cos(radians(latitude)) *
-            cos(radians(longitude) - radians(:longitude)) +
-            sin(radians(:latitude)) * sin(radians(latitude))
-        )) <= :radiusKm
-    ORDER BY distance
-    """,
-        nativeQuery = true)
-    fun findNearestMosques(
-        @Param("latitude") latitude: Double,
-        @Param("longitude") longitude: Double,
-        @Param("radiusKm") radiusKm: Double
-    ): List<Mosque>
 
-    fun searchMosquesByName(name: String): List<Mosque>
 
 }
