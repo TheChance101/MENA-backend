fa7da244b44df444bad75beb5f0914bbef5cbd22
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-10-29 15:51:30 +0300

    reformat search query

--- dukan/src/main/kotlin/net/thechance/dukan/repository/DukanSearchRepository.kt
+++ dukan/src/main/kotlin/net/thechance/dukan/repository/DukanSearchRepository.kt
@@ -8,18 +8,17 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import org.springframework.data.repository.query.Param
 
 interface DukanSearchRepository : ElasticsearchRepository<DukanDocument, String> {
-    fun findByNameContaining(query: String, pageable: Pageable): Page<DukanDocument>
-
-
-    @Query("""
-{
-  "wildcard": {
-    "name": {
-      "value": "*?0*",
-      "case_insensitive": true
+    @Query(
+        """
+    {
+      "wildcard": {
+        "name": {
+          "value": "*?0*",
+          "case_insensitive": true
+        }
+      }
     }
-  }
-}
-""")
+    """
+    )
     fun searchByNameLike(@Param("query") query: String, pageable: Pageable): Page<DukanDocument>
 }
