6f078a251f9e1857843b9b546f17dfc2929e8c86
Marwan Qashwa <marawankashwa24@gmail.com>
2025-10-29 17:41:58 +0300

    feet: add endpoint to retrieve prayer times by Hijri date

--- faith/src/main/kotlin/net/thechance/faith/api/controller/PrayerTimeController.kt
+++ faith/src/main/kotlin/net/thechance/faith/api/controller/PrayerTimeController.kt
@@ -7,6 +7,7 @@ import net.thechance.faith.service.PrayerService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDate
+import java.time.chrono.HijrahDate
 
 @RestController
 @RequestMapping("faith/prayer/times")
@@ -27,6 +28,29 @@ class PrayerTimeController(
         ).toResponse()
         return ResponseEntity.ok(prayerTimes)
     }
+
+    @GetMapping("hijri/{date}")
+    fun getPrayerTimesByHijri(
+        @PathVariable date: String,
+        @RequestParam latitude: Double,
+        @RequestParam longitude: Double
+    ): ResponseEntity<DayPrayerTimingsResponse> {
+        val prayerTimes = prayerService.getPrayerTimes(
+            latitude = latitude,
+            longitude = longitude,
+            date = date.hijriDateToLocalDate()
+        ).toResponse()
+        return ResponseEntity.ok(prayerTimes)
+    }
+
+    private fun String.hijriDateToLocalDate(): LocalDate = runCatching {
+        val parts = this.split("-")
+        val year = parts[0].toInt()
+        val month = parts[1].toInt()
+        val day = parts[2].toInt()
+        val hijriDate = HijrahDate.of(year, month, day)
+        LocalDate.from(hijriDate)
+    }.getOrElse { throw InvalidDateFormatException() }
 
     private fun String.toLocalDate(): LocalDate = runCatching {
         LocalDate.parse(this)
