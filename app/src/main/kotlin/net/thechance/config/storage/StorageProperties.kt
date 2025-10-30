b4fab3f97d06b6aa061e15d0716817be9b21b201
Hasan Ali <ihasanalihasan@gmail.com>
2025-10-30 14:02:49 +0300

    FTH-104: update application properties and add faith storage configuration

--- app/src/main/kotlin/net/thechance/config/storage/StorageProperties.kt
+++ app/src/main/kotlin/net/thechance/config/storage/StorageProperties.kt
@@ -7,7 +7,8 @@ data class AllStorageProperties(
     val mena: StorageProperties,
     val dukan: StorageProperties,
     val trends: StorageProperties,
-    val wallet: StorageProperties
+    val wallet: StorageProperties,
+    val faith: StorageProperties
 )
 
 data class StorageProperties(
