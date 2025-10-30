eab3bb1642065e20e7dff168cf51e037afe4ec6d
raouf-kamel <raoufkamel2013@gmail.com>
2025-10-30 03:23:53 +0300

    add sendMessageAudio endpoint to ChatController for handling audio message submissions

--- chat/src/main/kotlin/net/thechance/chat/api/controller/ChatController.kt
+++ chat/src/main/kotlin/net/thechance/chat/api/controller/ChatController.kt
@@ -65,7 +65,6 @@ class ChatController(
         )
     }
 
-
     @PostMapping("/image")
     fun sendMessageImage(
         @ModelAttribute request: MessageImageRequest,
@@ -86,6 +85,25 @@ class ChatController(
 
         return ResponseEntity.ok(message.toResponse(senderId))
     }
+
+    @PostMapping("/audio")
+    fun sendMessageAudio(
+        @ModelAttribute request: MessageAudioRequest,
+        principal: Principal
+    ): ResponseEntity<MessageResponse> {
+        val senderId = UUID.fromString(principal.name)
+        val messageAudioArgs = request.toRequestArgs(senderId)
+        val message = chatService.saveMessageAudio(messageAudioArgs)
+        chatService.getChatUsersIds(chatId = request.chatId).forEach { chatParticipantId ->
+            messagingTemplate.convertAndSendToUser(
+                chatParticipantId.toString(),
+                PRIVATE_MESSAGES,
+                message.toResponse(chatParticipantId)
+            )
+        }
+        return ResponseEntity.ok(message.toResponse(senderId))
+    }
+
 
     @MessageMapping("/chat.markAsRead")
     fun markMessagesAsRead(
