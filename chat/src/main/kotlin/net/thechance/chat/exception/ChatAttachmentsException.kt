f6ec1cd362b00d44dcffc1be3af984950b56971f
raouf-kamel <raoufkamel2013@gmail.com>
2025-10-30 03:21:32 +0300

    add custom exceptions for audio upload errors

--- chat/src/main/kotlin/net/thechance/chat/exception/ChatAttachmentsException.kt
+++ chat/src/main/kotlin/net/thechance/chat/exception/ChatAttachmentsException.kt
@@ -1,6 +1,8 @@
 package net.thechance.chat.exception
 
+import net.thechance.chat.exception.ErrorCodes.AUDIO_UPLOAD_FAILED
 import net.thechance.chat.exception.ErrorCodes.IMAGE_UPLOAD_FAILED
+import net.thechance.chat.exception.ErrorCodes.INVALID_AUDIO_FORMAT
 import net.thechance.chat.exception.ErrorCodes.INVALID_IMAGE_FORMAT
 import org.springframework.http.HttpStatus
 
@@ -14,4 +16,15 @@ class ImageUploadFailedException : ChatException(
     code = IMAGE_UPLOAD_FAILED,
     status = HttpStatus.INTERNAL_SERVER_ERROR,
     message = "Image uploading failed"
+)
+class InvalidAudioFormatException : ChatException(
+    code = INVALID_AUDIO_FORMAT,
+    status = HttpStatus.BAD_REQUEST,
+    message = "Invalid audio format"
+)
+
+class AudioUploadFailedException : ChatException(
+    code = AUDIO_UPLOAD_FAILED,
+    status = HttpStatus.INTERNAL_SERVER_ERROR,
+    message = "Audio uploading failed"
 )
--- chat/src/main/kotlin/net/thechance/chat/exception/ErrorCodes.kt
+++ chat/src/main/kotlin/net/thechance/chat/exception/ErrorCodes.kt
@@ -1,6 +1,9 @@
 package net.thechance.chat.exception
 
 object ErrorCodes {
-    const val INVALID_IMAGE_FORMAT = 1201
-    const val IMAGE_UPLOAD_FAILED = 1202
+    const val INVALID_IMAGE_FORMAT = "1201"
+    const val IMAGE_UPLOAD_FAILED = "1202"
+
+    const val INVALID_AUDIO_FORMAT = "INVALID_AUDIO_FORMAT"
+    const val AUDIO_UPLOAD_FAILED = "AUDIO_UPLOAD_FAILED"
 }
