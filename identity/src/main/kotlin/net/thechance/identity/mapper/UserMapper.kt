eba519c7672f1276260b867fe6d988ebf6e4d2fd
pola george <poulageorge61@gmail.com>
2025-10-30 19:41:36 +0300

    add profile image directory configuration and refactor image handling to use dynamic paths in S3 storage and responses

--- identity/src/main/kotlin/net/thechance/identity/mapper/UserMapper.kt
+++ identity/src/main/kotlin/net/thechance/identity/mapper/UserMapper.kt
@@ -3,13 +3,18 @@ package net.thechance.identity.mapper
 import net.thechance.identity.api.dto.ProfileResponse
 import net.thechance.identity.entity.User
 
-fun User.toResponse(cdnEndpoint: String): ProfileResponse {
+fun User.toResponse(imageBaseUrl: String): ProfileResponse {
+    val imageUrl1 = if (imageUrl.isNullOrBlank()) {
+        null
+    } else {
+        "$imageBaseUrl/$imageUrl"
+    }
     return ProfileResponse(
         id = id.toString(),
         username = username,
         firstName = firstName,
         lastName = lastName,
-        imageUrl = if (imageUrl.isNullOrBlank()) null else "$cdnEndpoint/$imageUrl",
+        imageUrl = imageUrl1,
         birthDate = birthDate.toString(),
         gender = gender
     )
