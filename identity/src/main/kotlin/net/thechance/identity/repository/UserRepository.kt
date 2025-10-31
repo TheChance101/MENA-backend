1357ebdc39276ee8e35ebd2fdf0758c98be8c0e1
edrees <muhammed.edrees101@gmail.com>
2025-10-31 18:20:28 +0200

    feat: add search functionality to UserRepository for user lookup by name or phone number

--- identity/src/main/kotlin/net/thechance/identity/repository/UserRepository.kt
+++ identity/src/main/kotlin/net/thechance/identity/repository/UserRepository.kt
@@ -1,9 +1,24 @@
 package net.thechance.identity.repository
 
 import net.thechance.identity.entity.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.util.*
 
 interface UserRepository: JpaRepository<User, UUID> {
     fun findByPhoneNumber(phoneNumber: String): User?
+
+    @Query("""
+    SELECT u FROM User u
+    WHERE 
+        LOWER(CONCAT(u.firstName, ' ', u.lastName)) LIKE LOWER(CONCAT('%', :query, '%'))
+        OR u.phoneNumber LIKE CONCAT('%', :query, '%')
+""")
+    fun searchUsers(
+        @Param("query") query: String,
+        pageable: Pageable
+    ): Page<User>
 }
