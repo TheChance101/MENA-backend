03e8e2f6dd848155f786ffa80b0ff71a7f0ccf0b
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-10-31 19:41:48 +0200

    feature: add RegisterController with the needed end points and its advice.

---
+++ identity/src/main/kotlin/net/thechance/identity/api/controller/register/RegisterControllerAdvice.kt
@@ -0,0 +1,72 @@
+package net.thechance.identity.api.controller.register
+
+import net.thechance.identity.api.dto.ErrorResponse
+import net.thechance.identity.exception.InvalidOtpException
+import net.thechance.identity.exception.InvalidPhoneNumberException
+import net.thechance.identity.exception.OtpExpiredException
+import net.thechance.identity.exception.UserAlreadyExistsException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice(assignableTypes = [RegisterController::class])
+@Order(1)
+class RegisterControllerAdvice {
+    private val logger: Logger = LoggerFactory.getLogger(RegisterController::class.java)
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationExceptions(exception: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        val errors = exception.bindingResult.fieldErrors.associate {
+            it.field to it.defaultMessage
+        }
+        logger.error("Validation failed: $errors", exception)
+        val errorResponse = ErrorResponse("Data not valid")
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(errorResponse)
+    }
+
+    @ExceptionHandler(InvalidPhoneNumberException::class)
+    fun handleInvalidPhoneNumberException(exception: InvalidPhoneNumberException): ResponseEntity<ErrorResponse> {
+        logger.error("Invalid phone number: ${exception.message}", exception)
+        val errorResponse = ErrorResponse(exception.message ?: "Invalid phone number")
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(errorResponse)
+    }
+
+    @ExceptionHandler(InvalidOtpException::class)
+    fun handleInvalidOtpException(exception: InvalidOtpException): ResponseEntity<ErrorResponse> {
+        logger.error("OTP is invalid: ${exception.message}", exception)
+        val errorResponse = ErrorResponse(exception.message ?: "OTP is invalid")
+        return ResponseEntity
+            .status(HttpStatus.UNAUTHORIZED)
+            .body(errorResponse)
+    }
+
+    @ExceptionHandler(OtpExpiredException::class)
+    fun handleOtpExpiredException(exception: OtpExpiredException): ResponseEntity<ErrorResponse> {
+        logger.error("OTP is expired: ${exception.message}", exception)
+        val errorResponse = ErrorResponse(exception.message ?: "OTP is expired")
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(errorResponse)
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException::class)
+    fun handleUserAlreadyExistsException(exception: UserAlreadyExistsException): ResponseEntity<ErrorResponse> {
+        logger.error("User Already Exists: ${exception.message}", exception)
+        val errorResponse = ErrorResponse(exception.message ?: "User Already Exists")
+        return ResponseEntity
+            .status(HttpStatus.CONFLICT)
+            .body(errorResponse)
+    }
+
+}
