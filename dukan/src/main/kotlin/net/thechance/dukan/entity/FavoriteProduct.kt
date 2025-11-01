315e9d00a4a91ed7d67aae6ff503de37638dad00
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-11-01 20:34:51 +0200

    feature: Implemented favorite product table and it's repository

---
+++ dukan/src/main/kotlin/net/thechance/dukan/entity/FavoriteProduct.kt
@@ -0,0 +1,33 @@
+package net.thechance.dukan.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.Instant
+import java.util.UUID
+
+@Table(name = "favorite_product", schema = "dukan")
+@Entity
+data class FavoriteProduct(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID = UUID.randomUUID(),
+
+    @Column(name = "owner_id", nullable = false)
+    val ownerId: UUID,
+
+    @Column(name = "product_id", nullable = false)
+    val productId: UUID,
+
+    @Column(name = "is_favorite", nullable = false)
+    val isFavorite: Boolean,
+
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant = Instant.now(),
+
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant = Instant.now()
+)
---
+++ dukan/src/main/kotlin/net/thechance/dukan/repository/FavoriteProductRepository.kt
@@ -0,0 +1,12 @@
+package net.thechance.dukan.repository
+
+import net.thechance.dukan.entity.FavoriteProduct
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface FavoriteProductRepository : JpaRepository<FavoriteProduct, UUID> {
+
+    fun findByOwnerIdAndProductId(ownerId: UUID, productId: UUID): FavoriteProduct?
+}
