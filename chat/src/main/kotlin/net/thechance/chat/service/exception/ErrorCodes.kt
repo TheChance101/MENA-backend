7b387b2f94d0657f08c38250c21aa67c9276257f
raouf-kamel <raoufkamel2013@gmail.com>
2025-11-03 19:10:45 +0300

    refactor: change error codes from strings to integers in ErrorCodes and ErrorResponse

--- chat/src/main/kotlin/net/thechance/chat/service/exception/ErrorCodes.kt
+++ chat/src/main/kotlin/net/thechance/chat/service/exception/ErrorCodes.kt
@@ -1,9 +1,9 @@
 package net.thechance.chat.service.exception
 
 object ErrorCodes {
-    const val INVALID_IMAGE_FORMAT = "1201"
-    const val IMAGE_UPLOAD_FAILED = "1202"
+    const val INVALID_IMAGE_FORMAT = 1201
+    const val IMAGE_UPLOAD_FAILED = 1202
 
-    const val INVALID_AUDIO_FORMAT = "INVALID_AUDIO_FORMAT"
-    const val AUDIO_UPLOAD_FAILED = "AUDIO_UPLOAD_FAILED"
+    const val INVALID_AUDIO_FORMAT = 1203
+    const val AUDIO_UPLOAD_FAILED = 1204
 }
