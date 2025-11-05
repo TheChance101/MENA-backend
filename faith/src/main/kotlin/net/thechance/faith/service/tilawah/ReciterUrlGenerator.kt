f8637b3b379d39acb6bf482e8e52b3f165e66898
Marwan Qashwa <marawankashwa24@gmail.com>
2025-11-05 18:11:13 +0200

    feat: add ReciterUrlGenerator for generating audio URLs

---
+++ faith/src/main/kotlin/net/thechance/faith/service/tilawah/ReciterUrlGenerator.kt
@@ -0,0 +1,17 @@
+package net.thechance.faith.service.tilawah
+
+import net.thechance.faith.entity.Reciter
+import net.thechance.faith.utils.padWithThreeDigits
+import org.springframework.stereotype.Component
+
+@Component
+class ReciterUrlGenerator {
+
+    fun generateAyahSoundUrl(surahNumber: Int, ayahNumber: Int, reciter: Reciter): String {
+        return "${reciter.serverUrl}${surahNumber.padWithThreeDigits()}${ayahNumber.padWithThreeDigits()}.mp3"
+    }
+
+    fun generateSurahSoundsUrl(surahNumber: Int, reciter: Reciter): String {
+        return "${reciter.serverUrl}/zips/${surahNumber.padWithThreeDigits()}.zip"
+    }
+}
