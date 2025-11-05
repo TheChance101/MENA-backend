106701565ac8ecd412ae551ab9feac567a330bf3
abdelrhman <abdelrhmanshabana999@gmail.com>
2025-11-05 09:50:20 +0200

    fix: add Service annotation

--- dukan/src/main/kotlin/net/thechance/dukan/service/FavouriteDukanService.kt
+++ dukan/src/main/kotlin/net/thechance/dukan/service/FavouriteDukanService.kt
@@ -3,8 +3,10 @@ package net.thechance.dukan.service
 import jakarta.transaction.Transactional
 import net.thechance.dukan.entity.FavoriteDukan
 import net.thechance.dukan.repository.FavoriteDukanRepository
+import org.springframework.stereotype.Service
 import java.util.*
 
+@Service
 class FavouriteDukanService(
     private val favoriteDukanRepository: FavoriteDukanRepository
 ) {
