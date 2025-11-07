afb9ebcb594764cbbf0f8704db89eddeec946764
Edrees <muhammed.edrees101@gmail.com>
2025-11-08 01:22:31 +0200

    Feature/wlt 118 handle blocked user (#159)

--- identity/src/test/kotlin/net/thechance/identity/service/UserServiceTest.kt
+++ identity/src/test/kotlin/net/thechance/identity/service/UserServiceTest.kt
@@ -2,6 +2,8 @@ package net.thechance.identity.service
 
 import com.google.common.truth.Truth.assertThat
 import io.mockk.*
+import net.thechance.events.identity.UserStatusUpdatedEvent
+import net.thechance.events.publisher.MenaEventPublisher
 import net.thechance.identity.entity.User
 import net.thechance.identity.exception.PasswordNotUpdatedException
 import net.thechance.identity.exception.UserNotFoundException
@@ -20,7 +22,15 @@ import java.util.*
 class UserServiceTest {
     private val userRepository: UserRepository = mockk(relaxed = true)
     private val identityImageStorageService: IdentityImageStorageService = mockk(relaxed = true)
-    private val userService = UserService(userRepository, identityImageStorageService, "profile-images")
+    private val authenticationService: AuthenticationService = mockk(relaxed = true)
+    private val eventPublisher: MenaEventPublisher = mockk(relaxed = true)
+    private val userService = UserService(
+        userRepository = userRepository,
+        identityImageStorageService = identityImageStorageService,
+        eventPublisher = eventPublisher,
+        authenticationService = authenticationService,
+        profileImageDirectory = "profile-images"
+    )
     private val mockImageFile: MultipartFile = mockk(relaxed = true)
 
     @Test
@@ -292,6 +302,27 @@ class UserServiceTest {
         userService.updateUserStatus(userId, newStatus)
 
         verify(exactly = 1) { userRepository.updateStatus(userId, newStatus) }
+    }
+
+    @Test
+    fun `updateUserStatus() should publish UserStatusUpdatedEvent when it updates successfully`() {
+        val newStatus = User.Status.ACTIVE
+        val event = UserStatusUpdatedEvent(userId, UserStatusUpdatedEvent.UserStatus.ACTIVE)
+        every{ userRepository.updateStatus(userId, newStatus) } returns 1
+
+        userService.updateUserStatus(userId, newStatus)
+
+        verify(exactly = 1) { eventPublisher.publish(event) }
+    }
+
+    @Test
+    fun `updateUserStatus() should logout user if user is blocked`() {
+        val newStatus = User.Status.BLOCKED
+        every{ userRepository.updateStatus(userId, newStatus) } returns 1
+
+        userService.updateUserStatus(userId, newStatus)
+
+        verify(exactly = 1) { authenticationService.logout(userId) }
     }
 
     @Test
--- wallet/src/main/kotlin/net/thechance/wallet/service/PaymentService.kt
+++ wallet/src/main/kotlin/net/thechance/wallet/service/PaymentService.kt
@@ -3,6 +3,7 @@ package net.thechance.wallet.service
 import net.thechance.wallet.entity.PendingTransaction
 import net.thechance.wallet.entity.Transaction
 import net.thechance.wallet.entity.toTransaction
+import net.thechance.wallet.exception.BlockedWalletUserException
 import net.thechance.wallet.repository.PendingTransactionRepository
 import net.thechance.wallet.repository.TransactionRepository
 import org.springframework.stereotype.Service
@@ -20,11 +21,24 @@ class PaymentService(
     @Transactional
     fun pay(userId: UUID, transactionId: UUID) {
         val pendingTransaction = validateAndGetTransaction(userId, transactionId)
-
+        val transactionStatus = getTransactionStatus(pendingTransaction)
         val block = blockService.getCurrentBlock()
-        val transaction = pendingTransaction.toTransaction(block, Transaction.Status.SUCCESS)
+
+        val transaction = pendingTransaction.toTransaction(block, transactionStatus)
         transactionRepository.save(transaction)
         pendingTransactionRepository.deleteById(transactionId)
+
+        if(transactionStatus == Transaction.Status.FAILED) {
+            throw BlockedWalletUserException("Either sender or receiver is blocked")
+        }
+    }
+
+    private fun getTransactionStatus(pendingTransaction: PendingTransaction): Transaction.Status {
+        return when {
+            pendingTransaction.sender.isBlocked() -> Transaction.Status.FAILED
+            pendingTransaction.receiver.isBlocked() -> Transaction.Status.FAILED
+            else -> Transaction.Status.SUCCESS
+        }
     }
 
     private fun validateAndGetTransaction(userId: UUID, transactionId: UUID): PendingTransaction {
