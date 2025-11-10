f392cc87d29bd8ba923def301c340bcd6457ab14
raouf-kamel <raoufkamel2013@gmail.com>
2025-11-10 04:40:34 +0300

    fix: update chat summary to use contact name and image URL

--- chat/src/main/kotlin/net/thechance/chat/service/ChatService.kt
+++ chat/src/main/kotlin/net/thechance/chat/service/ChatService.kt
@@ -155,11 +155,16 @@ class ChatService(
 
         val chatsSummaries = chats.map { chat ->
             val otherUser = chat.users.firstOrNull { it.id != userId }
+            val contact = otherUser?.let { contactService.getContactByOwnerIdAndContactUserId(userId, it.id) }
+            val chatName = getChatName(contact, otherUser)
+            val imageUrl = otherUser?.imageUrl.orEmpty()
+
             chat.toSummary(
-                userId,
-                otherUser,
-                lastMessages.firstOrNull { it.chatId == chat.id },
-                unreadCounts.firstOrNull { it.chatId == chat.id }?.unreadCount ?: 0
+                userId = userId,
+                chatName = chatName,
+                imageUrl = imageUrl,
+                lastMessage = lastMessages.firstOrNull { it.chatId == chat.id },
+                unreadCount = unreadCounts.firstOrNull { it.chatId == chat.id }?.unreadCount ?: 0
             )
         }
         return chatsSummaries
@@ -171,11 +176,15 @@ class ChatService(
         val otherUser = chat.users.firstOrNull { it.id != userId }
         val lastMessage = messageRepository.findTopByChatIdOrderBySentAtDesc(chatId)
         val unreadCount = chatRepository.findUnreadCountsForChats(listOf(chatId)).firstOrNull()?.unreadCount ?: 0
+        val contact = otherUser?.let { contactService.getContactByOwnerIdAndContactUserId(userId, it.id) }
+        val chatName = getChatName(contact, otherUser)
+        val imageUrl = otherUser?.imageUrl.orEmpty()
         return chat.toSummary(
-            userId,
-            otherUser,
-            lastMessage,
-            unreadCount
+            userId = userId,
+            chatName = chatName,
+            imageUrl = imageUrl,
+            lastMessage = lastMessage,
+            unreadCount = unreadCount
         )
     }
 
