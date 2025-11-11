7c293edd6c6fc0472a1e33c0bc8554f84629d834
Hager Abobakr <hagarabobakr1@gmail.com>
2025-11-12 01:43:35 +0200

    add AdminWalletController

--- wallet/src/main/kotlin/net/thechance/wallet/api/controller/AdminWalletController.kt
+++ wallet/src/main/kotlin/net/thechance/wallet/api/controller/AdminWalletController.kt
@@ -1,12 +1,9 @@
 package net.thechance.wallet.api.controller
 
-import net.thechance.wallet.api.dto.balance.BalanceResponse
 import net.thechance.wallet.api.dto.balance.DepositRequest
-import net.thechance.wallet.api.dto.balance.toBalanceResponse
-import net.thechance.wallet.service.BalanceService
+import net.thechance.wallet.service.AdminWalletService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,14 +11,16 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
-@RequestMapping("/wallet")
-class BalanceController(
-    private val balanceService: BalanceService
+@RequestMapping("/wallet/admin")
+class AdminWalletController(
+    private val adminWalletService: AdminWalletService
 ) {
-    @GetMapping("/balance")
-    fun getUserBalance(@AuthenticationPrincipal userId: UUID): ResponseEntity<BalanceResponse> {
-        val response = balanceService.getUserBalance(userId).toBalanceResponse()
-
+    @PostMapping("/balance/deposit")
+    fun adminDeposit(
+        @AuthenticationPrincipal adminId: UUID,
+        @RequestBody request: DepositRequest
+    ): ResponseEntity<Unit> {
+        val response = adminWalletService.deposit(userId = adminId, request = request)
         return ResponseEntity.ok(response)
     }
 }
