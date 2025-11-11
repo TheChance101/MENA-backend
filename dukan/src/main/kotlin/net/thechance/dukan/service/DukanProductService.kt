e9da20be43f86791ff8b49677d45e62256039ebe
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-11-11 03:49:51 +0300

    add: add dukanId to product document response result event and mappers

--- dukan/src/main/kotlin/net/thechance/dukan/service/DukanProductService.kt
+++ dukan/src/main/kotlin/net/thechance/dukan/service/DukanProductService.kt
@@ -85,7 +85,8 @@ class DukanProductService(
     private fun DukanProduct.toProductSaveEvent() = ProductEvent.Save(
         id = this.id.toString(),
         name = this.name,
-        description = this.description,
+        dukanName = this.description,
+        dukanId = this.dukan.id.toString(),
         mainImageUrl = this.imageUrls.firstOrNull().orEmpty(),
         price = this.price,
         shelfName = this.shelf.title
