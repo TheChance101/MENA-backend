e4b75a79b4a36afadc3f9e53366854d49d6b1160
Hager Abobakr <hagarabobakr1@gmail.com>
2025-11-12 01:37:09 +0200

    add adminDeposit function to BalanceController

--- wallet/src/main/kotlin/net/thechance/wallet/api/controller/BalanceController.kt
+++ wallet/src/main/kotlin/net/thechance/wallet/api/controller/BalanceController.kt
@@ -1,11 +1,14 @@
 package net.thechance.wallet.api.controller
 
 import net.thechance.wallet.api.dto.balance.BalanceResponse
+import net.thechance.wallet.api.dto.balance.DepositRequest
 import net.thechance.wallet.api.dto.balance.toBalanceResponse
 import net.thechance.wallet.service.BalanceService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -20,6 +23,15 @@ class BalanceController(
     fun getUserBalance(@AuthenticationPrincipal userId: UUID): ResponseEntity<BalanceResponse> {
         val response = balanceService.getUserBalance(userId).toBalanceResponse()
 
+        return ResponseEntity.ok(response)
+    }
+
+    @PostMapping("/admin/balance/deposit")
+    fun adminDeposit(
+        @AuthenticationPrincipal adminId: UUID,
+        @RequestBody request: DepositRequest
+    ): ResponseEntity<Unit> {
+        val response = balanceService.deposit(userId = adminId, request = request)
         return ResponseEntity.ok(response)
     }
 }
