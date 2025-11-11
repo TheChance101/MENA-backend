31363d25f43d1bd083b2c8a3f1f4df509005ade7
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-11-11 12:04:47 +0200

    feat: add toAdminResponse function to map DukanProduct to DukanProductAdminResponse

--- dukan/src/main/kotlin/net/thechance/dukan/api/mapper/product/DukanProductMapper.kt
+++ dukan/src/main/kotlin/net/thechance/dukan/api/mapper/product/DukanProductMapper.kt
@@ -1,6 +1,8 @@
 package net.thechance.dukan.api.mapper.product
 
+import net.thechance.dukan.api.dto.product.DukanProductAdminResponse
 import net.thechance.dukan.api.dto.product.DukanProductResponse
+import net.thechance.dukan.entity.DukanProduct
 import net.thechance.dukan.service.model.DukanProductWithFavoriteAndQuantity
 
 
@@ -16,3 +18,13 @@ fun DukanProductWithFavoriteAndQuantity.toResponse() = DukanProductResponse(
     isFavorite = this.isFavorite,
     dukanId = this.product.dukan.id
     )
+
+fun DukanProduct.toAdminResponse() = DukanProductAdminResponse(
+    id = this.id,
+    name = this.name,
+    price = this.price,
+    discountedPrice = null, //TODO
+    description = this.description,
+    imageUrls = this.imageUrls,
+    createdAt = this.createdAt,
+)
