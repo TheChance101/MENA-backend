ae5b20d708af2e7d88b2e68e8e29060e57cd7bef
Abdallah Bakhet <abakhet2010@gmail.com>
2025-11-12 16:18:57 +0200

    refactor: change post methods to get and add the required params in the query

--- faith/src/main/kotlin/net/thechance/faith/api/controller/TilawahController.kt
+++ faith/src/main/kotlin/net/thechance/faith/api/controller/TilawahController.kt
@@ -8,8 +8,6 @@ import net.thechance.faith.api.dto.tilawah.toResponse
 import net.thechance.faith.service.tilawah.TilawahService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -25,27 +23,25 @@ class TilawahController(
         return ResponseEntity.ok(reciters.toResponse())
     }
 
-    @PostMapping("/ayah/sound")
+    @GetMapping("/ayah/sound")
     fun getAyahSoundUrl(
-        @Valid @RequestBody
-        ayahSoundRequest: AyahSoundRequest
+        @Valid request: AyahSoundRequest,
     ): ResponseEntity<String> {
         val soundUrl = recitersService.getAyahSoundUrl(
-            reciterId = ayahSoundRequest.reciterId,
-            surahNumber = ayahSoundRequest.surahNumber,
-            ayahNumber = ayahSoundRequest.ayahNumber
+            reciterId = request.reciterId,
+            surahNumber = request.surahNumber,
+            ayahNumber = request.ayahNumber
         )
         return ResponseEntity.ok(soundUrl)
     }
 
-    @PostMapping("/surah/sound")
+    @GetMapping("/surah/sound")
     fun getSurahSoundUrl(
-        @Valid @RequestBody
-        surahSoundRequest: SurahSoundRequest
+        @Valid request: SurahSoundRequest,
     ): ResponseEntity<String> {
         val soundUrl = recitersService.getSurahSoundsUrl(
-            reciterId = surahSoundRequest.reciterId,
-            surahNumber = surahSoundRequest.surahNumber,
+            reciterId = request.reciterId,
+            surahNumber = request.surahNumber,
         )
         return ResponseEntity.ok(soundUrl)
     }
