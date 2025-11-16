ea597d50dad246e69cf271b433f3a3c80bb20f1e
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-11-16 20:30:49 +0200

    feature: add UserCategoryRepository.kt

---
+++ trends/src/main/kotlin/net/thechance/trends/repository/UserCategoryRepository.kt
@@ -0,0 +1,10 @@
+package net.thechance.trends.repository
+
+import net.thechance.trends.entity.UserCategories
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface UserCategoryRepository: JpaRepository<UserCategories, UUID>{
+    fun findByUserIdAndCategoryId(userId: UUID, categoryId: UUID): UserCategories?
+    fun findUserCategoriesByUserId(userId: UUID): MutableList<UserCategories>
+}
