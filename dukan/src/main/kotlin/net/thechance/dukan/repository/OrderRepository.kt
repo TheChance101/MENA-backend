4a65e3481c14055672e442ccd21c18419977b25a
abdelrhman <abdelrhmanshabana999@gmail.com>
2025-11-16 15:15:59 +0200

    feature: make order details repository

---
+++ dukan/src/main/kotlin/net/thechance/dukan/repository/OrderRepository.kt
@@ -0,0 +1,15 @@
+package net.thechance.dukan.repository
+
+import jakarta.transaction.Transactional
+import net.thechance.dukan.entity.Order
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import java.util.*
+
+interface OrderRepository : JpaRepository<Order, UUID> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE Order order SET order.status = :status WHERE order.id = :orderId")
+    fun updateOrderStatus(orderId: UUID, status: Order.OrderStatus): Int
+}
