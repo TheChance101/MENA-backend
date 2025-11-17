fc779e53f501cc5ab36fc67ce3acca108723842d
Al-Qassim <qassimali624@gmail.com>
2025-11-17 23:03:54 +0300

    fix: issue with contact user repository

--- chat/src/main/kotlin/net/thechance/chat/repository/ContactUserRepository.kt
+++ chat/src/main/kotlin/net/thechance/chat/repository/ContactUserRepository.kt
@@ -2,8 +2,11 @@ package net.thechance.chat.repository
 
 import net.thechance.chat.entity.ContactUser
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.UUID
 
 interface ContactUserRepository : JpaRepository<ContactUser, UUID> {
-    fun findPhoneNumberById(id: UUID): String?
+    @Query("SELECT c.phoneNumber FROM ContactUser c WHERE c.id = :id")
+    fun findPhoneNumberById(@Param("id") id: UUID): String?
 }
