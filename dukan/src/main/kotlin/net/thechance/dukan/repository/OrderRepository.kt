f83726d8480851e3ca62c077830f9ddf5b090b95
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-11-17 16:29:06 +0200

    refactor: removed the order tables

--- dukan/src/main/kotlin/net/thechance/dukan/repository/OrderRepository.kt
+++
@@ -1,7 +0,0 @@
-package net.thechance.dukan.repository
-
-import net.thechance.dukan.entity.Order
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
-
-interface OrderRepository : JpaRepository<Order, UUID>
--- dukan/src/main/kotlin/net/thechance/dukan/repository/PendingOrderRepository.kt
+++
@@ -1,9 +0,0 @@
-package net.thechance.dukan.repository
-
-import net.thechance.dukan.entity.PendingOrder
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
-
-interface PendingOrderRepository : JpaRepository<PendingOrder, UUID> {
-    fun findByTransactionId(transactionId: UUID): PendingOrder?
-}
