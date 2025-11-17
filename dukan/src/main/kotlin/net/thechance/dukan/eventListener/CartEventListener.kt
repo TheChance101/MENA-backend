d839414173690f8329834e9a0da739c8dfdf026d
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-11-17 17:02:13 +0200

    refactor: made sure that the cart only updates when the transaction is completed

--- dukan/src/main/kotlin/net/thechance/dukan/eventListener/CartEventListener.kt
+++ dukan/src/main/kotlin/net/thechance/dukan/eventListener/CartEventListener.kt
@@ -2,77 +2,43 @@ package net.thechance.dukan.eventListener
 
 import jakarta.transaction.Transactional
 import net.thechance.dukan.entity.Cart
-import net.thechance.dukan.entity.Order
-import net.thechance.dukan.entity.PendingOrder
-import net.thechance.dukan.entity.SoldProduct
 import net.thechance.dukan.repository.CartRepository
-import net.thechance.dukan.repository.OrderRepository
-import net.thechance.dukan.repository.PendingOrderRepository
-import net.thechance.dukan.repository.SoldProductRepository
+import net.thechance.dukan.repository.DukanRepository
 import net.thechance.dukan.service.exception.CartNotFoundException
+import net.thechance.dukan.service.exception.DukanNotFoundException
 import net.thechance.events.publisher.MenaEventPublisher
+import net.thechance.events.wallet.TransactionCompletedEvent
 import org.springframework.stereotype.Component
 import java.time.Instant
 import java.util.*
 
 @Component
 class CartEventListener(
-    private val pendingOrderRepository: PendingOrderRepository,
+    private val dukanRepository: DukanRepository,
     private val cartRepository: CartRepository,
-    private val orderRepository: OrderRepository,
-    private val soldProductRepository: SoldProductRepository,
     private val eventPublisher: MenaEventPublisher
 ) {
 
-    // TODO: inject with TransactionCompletedEvent
     @Transactional
-    fun handle() {
+    fun handle(
+        transactionCompletedEvent: TransactionCompletedEvent
+    ) {
+        val dukan = dukanRepository.findByOwnerId(transactionCompletedEvent.receiverId)
+            ?: throw DukanNotFoundException()
 
-        val pendingOrder = pendingOrderRepository.findByTransactionId(
-            UUID.randomUUID() // TODO: Replace with the transactionId comingg from TransactionCompletedEvent
-        ) ?: return
+        if (transactionCompletedEvent.status == TransactionCompletedEvent.TransactionStatus.SUCCESS) {
+            val cart = cartRepository.findByUserIdAndDukanIdAndIsOrderPurchasedFalse(
+                    transactionCompletedEvent.senderId,
+                    dukan.id
+                ) ?: throw CartNotFoundException()
 
-        createOrder(pendingOrder)
-        createSoldProducts(pendingOrder)
 
-        val cart =
-            cartRepository.findByUserIdAndDukanIdAndIsOrderPurchasedFalse(pendingOrder.userId, pendingOrder.dukanId)
-                ?: throw CartNotFoundException()
+            cart.isOrderPurchased = true
+            cart.updatedAt = Instant.now()
+            cartRepository.save(cart)
 
-        cart.isOrderPurchased = true
-        cart.updatedAt = Instant.now()
-        cartRepository.save(cart)
-
-        createCart(pendingOrder.userId, pendingOrder.dukanId)
-    }
-
-    private fun createSoldProducts(pending: PendingOrder) {
-        pending.items.forEach { product ->
-            soldProductRepository.save(
-                SoldProduct(
-                    productId = product.productId,
-                    dukanId = pending.dukanId,
-                    quantity = product.quantity
-                )
-            )
+            createCart(transactionCompletedEvent.senderId, dukan.id)
         }
-
-        pendingOrderRepository.delete(pending)
-    }
-
-    private fun createOrder(pending: PendingOrder) {
-        val order = Order(
-            userId = pending.userId,
-            dukanId = pending.dukanId,
-            address = pending.address,
-            longitude = pending.longitude,
-            latitude = pending.latitude,
-            totalPrice = pending.totalPrice,
-            totalProducts = pending.items.sumOf { it.quantity },
-            createdAt = Instant.now()
-        )
-
-        orderRepository.save(order)
     }
 
     private fun createCart(userId: UUID, dukanId: UUID): Cart {
