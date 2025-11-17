3ae5aa74051cda587be9b4bf932b4822fa6019c8
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-11-17 19:47:42 +0200

    Feature/dkn 136 order details endpoint (#209)
    * feature: add order details table
    
    * feature: make order details repository
    
    * feature: add order response
    
    * fix: table columns name changed
    
    * feature: add order controller and service with handle exceptions
    
    * feature: add order controller and service with handle exceptions
    
    * reactor: get order by order id
    
    * reactor: get order by order id
    
    * reactor: get dukan id form order
    
    * reactor: get dukan id form order
    
    * feature: add latitude and longitude for response
    
    * feature: add dukan latitude and longitude
    
    * refactor: rename from getOrderByIdAndDukanId to getOrderByIdAndUserId
    
    * refactor: add generated order number and order id
    
    * refactor: return time zone for response
    
    * refactor: move mapping to Controller
    
    * refactor: remove order state
    
    * refactor: make clas contain order and isDukanOwner

---
+++ dukan/src/main/kotlin/net/thechance/dukan/api/dto/order/OrderResponse.kt
@@ -0,0 +1,23 @@
+package net.thechance.dukan.api.dto.order
+
+import java.math.BigDecimal
+import java.util.*
+
+class OrderResponse(
+    val orderId: UUID,
+    val orderNumber: Long,
+    val time: String,
+    val orderItemResponse: List<OrderItemResponse>,
+    val discount: BigDecimal,
+    val platformFees: BigDecimal,
+    val totalAmount: BigDecimal,
+    val addersLine: String,
+    val customerLatitude: Double,
+    val customerLongitude: Double,
+    val dukanLatitude: Double,
+    val dukanLongitude: Double,
+    val customerName: String,
+    val customerNumber: String,
+    val customerImage: String? = null,
+    val isDukanOwner: Boolean
+)
