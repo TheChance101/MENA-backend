9453a0b54332204de4c4728f8060fc18d857be08
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-11-18 14:10:15 +0200

    refactor: modified the dukan user table

--- dukan/src/main/kotlin/net/thechance/dukan/entity/DukanUser.kt
+++ dukan/src/main/kotlin/net/thechance/dukan/entity/DukanUser.kt
@@ -1,30 +1,46 @@
 package net.thechance.dukan.entity
 
 import jakarta.persistence.*
+import java.time.Instant
 import java.util.*
 
 @Entity
 @Table(name = "users", schema = "dukan")
 data class DukanUser(
     @Id
+    @Column(name = "id", nullable = false, unique = true)
     val userId: UUID,
 
-    @Column(nullable = false)
+    @Column(name = "first_name", nullable = false)
     val firstName: String,
 
-    @Column(nullable = false)
+    @Column(name = "last_name", nullable = false)
     val lastName: String,
 
-    @Column(nullable = true)
+    @Column(name = "image_url", nullable = true)
     val imageUrl: String?,
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "phone_number", nullable = false, unique = true)
     val phoneNumber: String,
 
-    @OneToOne(cascade = [CascadeType.ALL], fetch = FetchType.EAGER)
-    @JoinColumn(name = "dukan_id", referencedColumnName = "dukanId")
+    @Column(name = "address", nullable = false)
+    var address: String = "",
+
+    @Column(name = "latitude", nullable = false)
+    var latitude: Double = 0.0,
+
+    @Column(name = "longitude", nullable = false)
+    var longitude: Double = 0.0,
+
+    @OneToOne(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    @JoinColumn(name = "dukanId", referencedColumnName = "id")
     val dukan: Dukan? = null,
 
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant = Instant.now(),
+
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant = Instant.now()
 ) {
     val userName: String
         get() = "$firstName $lastName"
