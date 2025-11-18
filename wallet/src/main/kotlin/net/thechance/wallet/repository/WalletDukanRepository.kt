06a766817c976f9c8c03c6775b484824106d5298
Malak Raef <malak.raef20@gmail.com>
2025-11-18 18:01:06 +0200

    optimize imports

--- wallet/src/main/kotlin/net/thechance/wallet/repository/WalletDukanRepository.kt
+++ wallet/src/main/kotlin/net/thechance/wallet/repository/WalletDukanRepository.kt
@@ -2,9 +2,6 @@ package net.thechance.wallet.repository
 
 import net.thechance.wallet.entity.WalletDukan
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
-import java.util.UUID
+import java.util.*
 
 interface WalletDukanRepository: JpaRepository<WalletDukan, UUID>
