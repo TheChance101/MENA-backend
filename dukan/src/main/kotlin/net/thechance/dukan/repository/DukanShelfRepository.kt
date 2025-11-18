188658e85769d1978b0f0b1da73a0ccba8f01aae
abdelrhman <abdelrhmanshabana999@gmail.com>
2025-11-18 16:54:10 +0200

    refactor: add query to join dukan product table with shelf table by id

--- dukan/src/main/kotlin/net/thechance/dukan/repository/DukanShelfRepository.kt
+++ dukan/src/main/kotlin/net/thechance/dukan/repository/DukanShelfRepository.kt
@@ -4,13 +4,27 @@ import net.thechance.dukan.entity.DukanShelf
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-import java.util.UUID
+import java.util.*
 
 @Repository
 interface DukanShelfRepository : JpaRepository<DukanShelf, UUID> {
     fun existsByTitleAndDukanId(title: String, dukanId: UUID): Boolean
     fun findAllByDukanId(dukanId: UUID): List<DukanShelf>
-    fun findAllByDukanId(dukanId: UUID, pageable: Pageable): Page<DukanShelf>
     fun findByIdAndDukanId(id: UUID, dukanId: UUID): DukanShelf?
+
+    @Query(
+        """
+    SELECT dukanShelf FROM DukanShelf dukanShelf 
+    JOIN DukanProduct dukanProduct ON dukanProduct.shelf.id = dukanShelf.id 
+    WHERE dukanShelf.dukan.id = :dukanId 
+    AND dukanProduct.isDeleted = false
+    GROUP BY dukanShelf
+"""
+    )
+    fun findAllByDukanId(
+        dukanId: UUID,
+        pageable: Pageable
+    ): Page<DukanShelf>
 }
