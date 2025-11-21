8fc0cc787e9122d87fe7db19e2cb2d478e268c87
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-11-21 03:26:49 +0300

    rename: Event -> SearchEvent

--- events/src/main/kotlin/net/thechance/events/dukan/DukanSearchEvent.kt
+++ events/src/main/kotlin/net/thechance/events/dukan/DukanSearchEvent.kt
@@ -2,7 +2,7 @@ package net.thechance.events.dukan
 
 import net.thechance.events.MenaEvent
 
-sealed class DukanEvent : MenaEvent {
+sealed class DukanSearchEvent : MenaEvent {
     data class Save(
         val id: String,
         val name: String,
@@ -10,14 +10,19 @@ sealed class DukanEvent : MenaEvent {
         val imageUrl:String?,
         val lat:Double,
         val lng:Double,
-        val categoryIds:Set<String>
-    ):DukanEvent() {
+        val categoryIds:Set<String>,
+        val activationStatus: ActivationStatus
+    ):DukanSearchEvent() {
         enum class Status {
             APPROVED,
             REJECTED,
             PENDING,
         }
+        enum class ActivationStatus {
+            ACTIVATED,
+            DEACTIVATED
+        }
     }
-    data class Delete(val id:String):DukanEvent()
+    data class Delete(val id:String):DukanSearchEvent()
 }
 
